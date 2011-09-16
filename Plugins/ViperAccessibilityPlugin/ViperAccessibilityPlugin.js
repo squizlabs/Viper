26ee558d459b1ff382e053fcbf8edfc4b40b9c6e
Sertan Danis <sdanis@squiz.com.au>
2011-09-16 15:35:50 +1000

    Initial commit of the ViperAccessibilityPlugin

---
+++ Plugins/ViperAccessibilityPlugin/ViperAccessibilityPlugin.js
@@ -0,0 +1,33 @@
+/**
+ * JS Class for the Viper Toolbar Plugin.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program as the file license.txt. If not, see
+ * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
+ *
+ * @package    CMS
+ * @subpackage Editing
+ * @author     Squiz Pty Ltd <products@squiz.net>
+ * @copyright  2010 Squiz Pty Ltd (ACN 084 670 600)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
+ */
+
+function ViperAccessibilityPlugin(viper)
+{
+    this.viper = viper;
+
+}
+
+ViperAccessibilityPlugin.prototype = {
+
+
+};
