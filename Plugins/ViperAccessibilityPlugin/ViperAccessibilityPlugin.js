dbabf5c47c7f111e054f7b4f9bccb00452c9236b
Sertan Danis <sdanis@squiz.com.au>
2011-12-22 15:34:16 +1100

    Added settings panel to Accessibility plugin.

--- Plugins/ViperAccessibilityPlugin/ViperAccessibilityPlugin.js
+++ Plugins/ViperAccessibilityPlugin/ViperAccessibilityPlugin.js
@@ -28,13 +28,23 @@ function ViperAccessibilityPlugin(viper)
     this._issueDetailsWrapper = null;
     this._resultsMiddle       = null;
     this._issueCount          = 0;
+    this._errorCount          = 0;
+    this._warningCount        = 0;
+    this._noticeCount         = 0;
     this._currentIssue        = 1;
     this._currentList         = 1;
+    this._subSection          = null;
     this._prevIssueBtn        = null;
     this._nextIssueBtn        = null;
+    this._checkContentBtn     = null;
+    this._aaTools             = null;
     this._toolbar             = null;
     this._listPageCounter     = null;
+    this._mainPanelLeft       = null;
     this._htmlCSsrc           = 'https://raw.github.com/squizlabs/HTML_CodeSniffer/master/HTMLCS.js';
+    this._standard            = 'WCAG2AAA';
+
+    this._htmlCSsrc = 'file:///Users/sdanis/Sites/HTML_CodeSniffer/HTMLCS.js';
 
 }
 
@@ -50,7 +60,7 @@ ViperAccessibilityPlugin.prototype = {
     {
         var self       = this;
         var _runChecks = function() {
-            HTMLCS.process('WCAG2AAA', self.viper.getViperElement(), callback);
+            HTMLCS.process(self._standard, self.viper.getViperElement(), callback);
         };
 
         if (!window.HTMLCS) {
@@ -81,6 +91,7 @@ ViperAccessibilityPlugin.prototype = {
         // Create the sub section and set it as active as its always visible.
         var subSectionCont = this._createSubSection();
         var subSection     = toolbar.createSubSection(subSectionCont, false);
+        this._subSection   = subSection;
 
         var toolsSection   = document.createElement('div');
         this._toolsSection = toolsSection;
@@ -92,6 +103,7 @@ ViperAccessibilityPlugin.prototype = {
 
         var aaTools = toolbar.createToolsPopup('Accessibility Auditor', toolsSection, [subSection]);
         dfx.setStyle(aaTools.element, 'width', '320px');
+        this._aaTools = aaTools;
 
         // Check button panel.
         // Check panel will show a check button which will start checking issues.
@@ -99,51 +111,40 @@ ViperAccessibilityPlugin.prototype = {
         dfx.addClass(checkPanel, 'ViperAP-tools ViperAP-checkTools');
         toolsSection.appendChild(checkPanel);
 
-        var _updateResults = function() {
-            // Show loading sub section only.
-            dfx.setStyle(dfx.getClass('ViperAP-cont', subSection), 'display', 'none');
-            dfx.setStyle(dfx.getClass('loadingCont', subSection)[0], 'display', 'block');
-
-            // Set the sub section to be visible.
-            dfx.addClass(subSection, 'active');
-            dfx.addClass(aaTools.element, 'subSectionVisible');
-
-            // Run the HTMLCS checks.
-            self.runChecks(function() {
-                // Hide loading sub section and show the main panel and results panel.
-                dfx.removeClass(toolsSection, 'checkTools');
-                dfx.addClass(toolsSection, 'listTools');
-
-                // Get the messages from HTMLCS.
-                var msgs = HTMLCS.getMessages();
-
-                // Hide the loading container.
-                dfx.setStyle(dfx.getClass('loadingCont', subSection)[0], 'display', 'none');
-
-                if (msgs.length === 0) {
-                    // No messages, show no results message.
-                    dfx.setStyle(dfx.getClass('noResultsCont', subSection)[0], 'display', 'block');
-                } else {
-                    // There are messages so update the issue list.
-                    self._updateIssues(msgs);
-                    dfx.setStyle(dfx.getClass('resultsCont', subSection)[0], 'display', 'block');
-                }
-            });
-        };
-
         var checkContentBtn = toolbar.createButton('Check Content', false, 'Check Content', false, '', function() {
             self._toolbar.disableButton(checkContentBtn);
-             _updateResults();
+             self.updateResults();
         }, null, null, checkPanel);
         checkPanel.appendChild(checkContentBtn);
+        this._checkContentBtn = checkContentBtn;
 
+        // Error info, settings and rerun button.
         toolsSection.appendChild(mainPanel);
-        var showAllBtn = toolbar.createButton('All', false, 'Show All', false, '', null, null, null, mainPanel);
-        mainPanel.appendChild(showAllBtn);
-        var showAllBtn = toolbar.createButton('', false, 'Re-run', false, 'accessRerun', function() {
-            _updateResults();
+
+        var mainPanelLeft   = document.createElement('div');
+        this._mainPanelLeft = mainPanelLeft;
+        dfx.addClass(mainPanelLeft, 'ViperAP-listTools-left');
+        mainPanel.appendChild(mainPanelLeft);
+        var mainPanelRight = document.createElement('div');
+        dfx.addClass(mainPanelRight, 'ViperAP-listTools-right');
+        mainPanel.appendChild(mainPanelRight);
+
+        var settingsBtn = toolbar.createButton('St', false, 'Settings', false, '', function() {
+            var resultsCont = dfx.getClass('resultsCont', subSection)[0];
+            if (dfx.getStyle(resultsCont, 'display') === 'none') {
+                dfx.setStyle(dfx.getClass('settingsCont', subSection)[0], 'display', 'none');
+                dfx.setStyle(dfx.getClass('resultsCont', subSection)[0], 'display', 'block');
+            } else {
+                dfx.setStyle(dfx.getClass('resultsCont', subSection)[0], 'display', 'none');
+                dfx.setStyle(dfx.getClass('settingsCont', subSection)[0], 'display', 'block');
+            }
+
         }, null, null, mainPanel);
-        mainPanel.appendChild(showAllBtn);
+        mainPanelRight.appendChild(settingsBtn);
+        var reRunBtn = toolbar.createButton('', false, 'Re-run', false, 'accessRerun', function() {
+            self.updateResults();
+        }, null, null, mainPanel);
+        mainPanelRight.appendChild(reRunBtn);
 
         toolbar.createButton('', false, 'Accessibility Auditor', false, 'accessAudit', null, null, aaTools);
 
@@ -193,6 +194,42 @@ ViperAccessibilityPlugin.prototype = {
         detailTools.appendChild(prevNextGroup);
         this._prevIssueBtn = prevButton;
         this._nextIssueBtn = nextButton;
+
+    },
+
+    updateResults: function()
+    {
+        var self = this;
+
+        // Show loading sub section only.
+        dfx.setStyle(dfx.getClass('ViperAP-cont', this._subSection), 'display', 'none');
+        dfx.setStyle(dfx.getClass('loadingCont', this._subSection)[0], 'display', 'block');
+
+        // Set the sub section to be visible.
+        dfx.addClass(this._subSection, 'active');
+        dfx.addClass(this._aaTools.element, 'subSectionVisible');
+
+        // Run the HTMLCS checks.
+        this.runChecks(function() {
+            // Hide loading sub section and show the main panel and results panel.
+            dfx.removeClass(self._toolsSection, 'checkTools');
+            dfx.addClass(self._toolsSection, 'listTools');
+
+            // Get the messages from HTMLCS.
+            var msgs = HTMLCS.getMessages();
+
+            // Hide the loading container.
+            dfx.setStyle(dfx.getClass('loadingCont', self._subSection)[0], 'display', 'none');
+
+            if (msgs.length === 0) {
+                // No messages, show no results message.
+                dfx.setStyle(dfx.getClass('noResultsCont', self._subSection)[0], 'display', 'block');
+            } else {
+                // There are messages so update the issue list.
+                self._updateIssues(msgs);
+                dfx.setStyle(dfx.getClass('resultsCont', self._subSection)[0], 'display', 'block');
+            }
+        });
 
     },
 
@@ -266,6 +303,9 @@ ViperAccessibilityPlugin.prototype = {
         // Results Container.
         main.appendChild(this._createResultsContainer());
 
+        // The settings container.
+        main.appendChild(this._createSettingsContainer());
+
         return main;
 
     },
@@ -305,6 +345,62 @@ ViperAccessibilityPlugin.prototype = {
 
         return results;
 
+    },
+
+    _createSettingsContainer: function()
+    {
+        var div = document.createElement('div');
+        dfx.addClass(div, 'settingsCont ViperAP-wrapper ViperAP-cont');
+        dfx.setStyle(div, 'display', 'none');
+
+        // List Filters.
+        var listFilters = document.createElement('div');
+        dfx.addClass(listFilters, 'listFilters');
+        dfx.setHtml(listFilters, '<h1>List Filters</h1><p>Errors and Warnings are always shown and cannot be hidden. Notices will be automatically shown if there are not other issues.</p>');
+        var includeNoticesChbox = ViperTools.createCheckbox('Always include Notices', true);
+        listFilters.appendChild(includeNoticesChbox);
+        div.appendChild(listFilters);
+
+        // Accessibility Standard.
+        var standards = document.createElement('div');
+        dfx.addClass(standards, 'accessStandard');
+        dfx.setHtml(standards, '<h1>Accessibility Standard</h1><p>Choose which standard you would like to check your content against.</p>');
+        div.appendChild(standards);
+
+        var trippleA = ViperTools.createRadiobutton('standard', 'WCAG2AAA', 'Tripple A (WCAG 2.0 AAA)', true);
+        var doubleA  = ViperTools.createRadiobutton('standard', 'WCAG2AA', 'Double A (WCAG 2.0 AA)');
+        var singleA  = ViperTools.createRadiobutton('standard', 'WCAG2A', 'Single A (WCAG 2.0 A)');
+        var sec508   = ViperTools.createRadiobutton('standard', 'Section508', 'Section 508 (U.S. Federal Agencies 1998)');
+
+        standards.appendChild(trippleA);
+        standards.appendChild(doubleA);
+        standards.appendChild(singleA);
+        standards.appendChild(sec508);
+
+        // Re check button.
+        var self       = this;
+        var reCheckSec = document.createElement('div');
+        dfx.addClass(reCheckSec, 'reCheckSec');
+        var reCheck = ViperTools.createButton('Re-check Content', false, 'Re-check Content', false, '', function() {
+            var radioBtns = document.getElementsByName('standard');
+            var value     = null;
+            for (var i = 0; i < radioBtns.length; i++) {
+                if (radioBtns[i].checked === true) {
+                    value = radioBtns[i].value;
+                    break;
+                }
+            }
+
+            if (value !== null) {
+                self._standard = value;
+                self.updateResults();
+            }
+        });
+        reCheckSec.appendChild(reCheck);
+
+        div.appendChild(reCheckSec);
+
+        return div;
     },
 
     _showIssueDetails: function(issue, li)
@@ -355,21 +451,24 @@ ViperAccessibilityPlugin.prototype = {
         dfx.empty(this._issueList);
         dfx.empty(this._issueDetailsWrapper);
 
-        var c = msgs.length;
+        var self           = this;
+        var c              = msgs.length;
+        var list           = null;
+        var firstList      = null;
+        var pages          = 0;
         this._issueCount   = c;
         this._currentIssue = 1;
-
-        var self          = this;
-        var list          = null;
-        var pages         = 0;
-        var firstList     = null;
-        this._currentList = 1;
+        this._currentList  = 1;
+        this._errorCount   = 0;
+        this._warningCount = 0;
+        this._noticeCount  = 0;
 
         // Create list inner wrapper.
         var listsInner = document.createElement('div');
         dfx.addClass(listsInner, 'ViperAP-issueListInner');
         this._issueList.appendChild(listsInner);
 
+        // Create multiple issue lists for pagination.
         for (var i = 0; i < c; i++) {
             if ((i % 5) === 0) {
                 list = document.createElement('ol');
@@ -386,6 +485,8 @@ ViperAccessibilityPlugin.prototype = {
             list.appendChild(this._createIssue(msg));
         }
 
+        // Set the width to the width of panel x number of pages so they are placed
+        // side by side for the sliding effect.
         dfx.setStyle(listsInner, 'width', (pages * 320) + 'px');
 
         // Create the list navigation buttons.
@@ -396,6 +497,7 @@ ViperAccessibilityPlugin.prototype = {
             var pageCounter       = document.createElement('span');
             this._listPageCounter = pageCounter;
 
+            // Show previous list of issues.
             var prev = document.createElement('span');
             dfx.addClass(prev, 'ViperAP-listNav-prev');
             listNav.appendChild(prev);
@@ -415,6 +517,7 @@ ViperAccessibilityPlugin.prototype = {
             dfx.setHtml(pageCounter, 'Pg 1 of ' + pages);
             listNav.appendChild(pageCounter);
 
+            // Show next list of issues.
             var next = document.createElement('span');
             dfx.addClass(next, 'ViperAP-listNav-next');
             listNav.appendChild(next);
@@ -437,6 +540,29 @@ ViperAccessibilityPlugin.prototype = {
         dfx.setStyle(this._resultsMiddle, 'width', ((c * 320) + 320) + 'px');
         dfx.setStyle(this._issueDetailsWrapper, 'width', (c * 320) + 'px');
 
+        // Update the number of issues.
+        this._updateNumberOfIssuesContainer();
+
+    },
+
+    _updateNumberOfIssuesContainer: function()
+    {
+        var content = '';
+
+        if (this._errorCount > 0) {
+            content += '<strong>' + this._errorCount + '</strong> Errors <span class="ViperAP-divider"></span>';
+        }
+
+        if (this._warningCount > 0) {
+            content += '<strong>' + this._warningCount + '</strong> Warnings <span class="ViperAP-divider"></span>';
+        }
+
+        if (this._noticeCount > 0) {
+            content += '<strong>' + this._noticeCount + '</strong> Notices';
+        }
+
+        dfx.setHtml(this._mainPanelLeft, content);
+
     },
 
     setCurrentListIndex: function(index)
@@ -454,6 +580,24 @@ ViperAccessibilityPlugin.prototype = {
         dfx.addClass(li, 'ViperAP-issueItem');
 
         var issueType = this._getIssueType(msg);
+
+        switch (msg.type) {
+            case HTMLCS.ERROR:
+                this._errorCount++;
+            break;
+
+            case HTMLCS.WARNING:
+                this._warningCount++;
+            break;
+
+            case HTMLCS.NOTICE:
+                this._noticeCount++;
+            break;
+
+            default:
+                // Unknown type.
+            break;
+        }//end switch
 
         var liContent = '<span class="ViperAP-issueType ' + issueType + '"></span>';
         liContent    += '<span class="ViperAP-issueTitle">' + dfx.ellipsize(msg.msg, 140) + '</span>';
@@ -480,14 +624,24 @@ ViperAccessibilityPlugin.prototype = {
 
         var issueType = this._getIssueType(issue);
 
+        var code = this._parseCode(issue.code);
+
         var content = '<div class="ViperAP-issueDetails">';
         content    += '<span class="ViperAP-issueType ' + issueType + '"></span>';
         content    += '<div class="issueTitle">' + issue.msg + '</div>';
         content    += '<div class="issueWcag">';
-        content    += '<strong>WCAG References</strong><br>';
-        content    += '<em>Category: </em> <a href="#">Non-text Content</a><br>';
-        content    += '<em>Technique: </em> <a href="#">H28</a><br>';
-        content    += '</div></div><!-- End References -->';
+        content    += '<strong>' + code.standard + ' References</strong><br>';
+        content    += '<em>Principle: </em> <a target="_blank" href="http://www.w3.org/TR/2008/REC-WCAG20-20081211/#' + code.principle + '">' + dfx.ucFirst(code.principle) + '</a><br>';
+        content    += '<em>Techniques: </em> ';
+
+        var techStrs = [];
+        for (var i = 0; i < code.techniques.length; i++) {
+            techStrs.push('<a target="_blank" href="http://www.w3.org/TR/2010/NOTE-WCAG20-TECHS-20101014/' + code.techniques[i] + '">' + code.techniques[i] + '</a>');
+        }
+
+        content += techStrs.join(', ');
+        content += '<br>';
+        content += '</div></div><!-- End References -->';
         dfx.setHtml(main, content);
 
         var resolutionCont = document.createElement('div');
@@ -501,7 +655,10 @@ ViperAccessibilityPlugin.prototype = {
         var resolutionHeader = dfx.getClass('resolutionHeader', resolutionCont)[0];
 
         // Create resolution tools.
-        var locateBtn     = this._toolbar.createButton('', false, 'Locate Element', false, 'locate', function() {}, null, null, resolutionHeader);
+        var self          = this;
+        var locateBtn     = this._toolbar.createButton('', false, 'Locate Element', false, 'locate', function() {
+            self.pointToElement(issue.element);
+        }, null, null, resolutionHeader);
         var sourceViewBtn = this._toolbar.createButton('', false, 'Show in Source View', false, 'sourceView', function() {}, null, null, resolutionHeader);
         var doneBtn = this._toolbar.createButton('Done', false, 'Mark as done', false, '', function() {}, null, null, resolutionHeader);
 
@@ -515,6 +672,72 @@ ViperAccessibilityPlugin.prototype = {
 
     },
 
+    pointToElement: function(element)
+    {
+        if (!element) {
+            return;
+        }
+
+        var rect = dfx.getBoundingRectangle(element);
+
+        var highlight = document.createElement('div');
+        dfx.addClass(highlight, 'ViperAP-highlight');
+        dfx.setStyle(highlight, 'left', rect.x1 + 'px');
+        dfx.setStyle(highlight, 'width', (rect.x2 - rect.x1) + 'px');
+        dfx.setStyle(highlight, 'top', rect.y1 + 'px');
+        dfx.setStyle(highlight, 'height', (rect.y2 - rect.y1) + 'px');
+
+        document.body.appendChild(highlight);
+        setTimeout(function() {
+            dfx.remove(highlight);
+        }, 2000);
+    },
+
+    _parseCode: function(code)
+    {
+        var sections = code.split('.');
+        var parsed   = {};
+        parsed.standard = sections[0];
+
+        if (sections[1].indexOf('Principle') === 0) {
+            var principle = sections[1].replace('Principle', '');
+            switch (principle) {
+                case '1':
+                    principle = 'perceivable';
+                break;
+
+                case '2':
+                    principle = 'operable';
+                break;
+
+                case '3':
+                    principle = 'understandable';
+                break;
+
+                case '4':
+                    principle = 'robust';
+                break;
+            }
+
+            parsed.principle = principle;
+        }
+
+        if (sections[2].indexOf('Guideline') === 0) {
+            parsed.guideline = sections[2].replace('Guideline', '').replace('_', '.');
+        }
+
+        parsed.section = sections[3].replace('_', '.');
+
+        parsed.techniques = [];
+
+        var techniques = sections[4].split(',');
+        for (var i = 0; i < techniques.length; i++) {
+            parsed.techniques.push(techniques[i]);
+        }
+
+        return parsed;
+
+    },
 
     _addResolutionContent: function(issue, detailsElement)
     {
