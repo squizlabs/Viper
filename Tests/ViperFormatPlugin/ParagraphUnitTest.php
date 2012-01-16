e38ef5c5dabe5ca5e4a35adce82f9727d60a7b6f
Deborah Sherwood <dsherwood@squiz.com.au>
2012-01-17 09:30:02 +1100

    Added a new unit test file for paragraph

---
+++ Tests/ViperFormatPlugin/ParagraphUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'AbstractViperUnitTest.php';
+
+class Viper_Tests_ViperFormatPlugin_ParagraphUnitTest extends AbstractViperUnitTest
+{
+
+
+    /**
+     * Tests that applying styles to whole paragraph and selecting the P in lineage shows paragraph tools.
+     *
+     * @return void
+     */
+    public function testSelectParaAfterStylingShowsCorrectIcons()
+    {
+        $dir = dirname(__FILE__).'/Images/';
+
+        $this->selectText('Lorem', 'dolor');
+        $this->keyDown('Key.CMD + b');
+        $this->keyDown('Key.CMD + i');
+
+        $this->selectInlineToolbarLineageItem(0);
+
+        // Make sure the correct icons are being shown.
+
+        $this->assertTrue($this->inlineToolbarButtonExists($dir.'toolbarIcon_toggle_formats_subActive.png'), 'Toogle formats icon is not selected');
+        $this->assertTrue($this->inlineToolbarButtonExists($dir.'toolbarIcon_p_active.png'), 'P icon is not active');
+
+    }//end testSelectParaAfterStylingShowsCorrectIcons()
+
+
+     /**
+     * Tests selecting text in a paragraph.
+     *
+     * @return void
+     */
+    public function testSelectingParagraphsWithFormattedTextShowsCorrectIcons()
+    {
+        $dir = dirname(__FILE__).'/Images/';
+
+        $this->selectText('Lorem', 'dolor');
+        $this->assertTrue($this->inlineToolbarButtonExists($dir.'toolbarIcon_toggle_formats_subActive.png'), 'Toogle formats icon is not selected');
+
+        $this->selectText('sit', 'WoW');
+        $this->assertTrue($this->inlineToolbarButtonExists($dir.'toolbarIcon_toggle_formats_subActive.png'), 'Toogle formats icon is not selected');
+
+    }//end testSelectingParagraphsWithFormattedTextShowsCorrectIcons()
+
+}//end class
+
+?>
