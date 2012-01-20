5be79032ab1e4eec00919dbe27d4ba07d2e11335
Deborah Sherwood <dsherwood@squiz.com.au>
2012-01-20 17:13:37 +1100

    Added test for rows in tables

--- Tests/ViperTableEditorPlugin/RowUnitTest.php
+++ Tests/ViperTableEditorPlugin/RowUnitTest.php
@@ -21,11 +21,65 @@ class Viper_Tests_ViperTableEditorPlugin_RowUnitTest extends AbstractViperTableE
 
 
     /**
-     * Test that creating a new table works.
+     * Test adding a new table and then adding new rows.
      *
      * @return void
      */
-    public function testRowInsert()
+    public function testAddingAndDeletingRowsInANewTable()
+    {
+        $this->insertTable();
+
+        $this->showTools(2, 'row');
+
+        //Add a new row after the first row of the table
+        $this->click($this->find($this->getImg('icon_insertRowAfter.png'), NULL, 0.83));
+
+        $struct   = $this->getTableStructure();
+        $expected = array(
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                    );
+
+        $this->assertTableStructure($expected, $struct);
+
+        //Add a new row before the first row of the table
+        $this->click($this->find($this->getImg('icon_insertRowBefore.png'), NULL, 0.83));
+
+        $struct   = $this->getTableStructure();
+        $expected = array(
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                    );
+
+        $this->assertTableStructure($expected, $struct);
+
+        //Delete the row
+        $this->click($this->find($this->getImg('icon_trash.png'), NULL, 0.83));
+
+        $struct   = $this->getTableStructure();
+        $expected = array(
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                     array(array(), array(), array()),
+                    );
+
+        $this->assertTableStructure($expected, $struct);
+
+    }//end testAddingAndDeletingRowsInANewTable()
+
+
+    /**
+     * Test that the 'All Genders' rowspan changes to three when you add a new row and goes back to two when you delete a new row.
+     *
+     * @return void
+     */
+    public function testRowspanChangesWhenNewRowAdded()
     {
         $this->showTools(0, 'row');
 
@@ -76,7 +130,87 @@ class Viper_Tests_ViperTableEditorPlugin_RowUnitTest extends AbstractViperTableE
 
         $this->assertTableStructure($expected, $struct);
 
-    }//end testRowInsert()
+    }//end testRowspanChangesWhenNewRowAdded()
+
+
+    /**
+     * Test that the 'All Genders' rowspan changes when you delete the first row from it.
+     *
+     * @return void
+     */
+    public function testRowspanChangesWhenYouDeleteTheFirstRow()
+    {
+        $this->showTools(2, 'row');
+
+        $this->click($this->find($this->getImg('icon_trash.png'), NULL, 0.83));
+
+        $this->assertHTMLMatch('<p>Lorem IPSUM dolor</p>
+<p>sit amet <strong>consectetur</strong></p>
+<table style="width: 300px;" border="1" cellspacing="2" cellpadding="2">
+    <tbody>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>All Genders</td>
+            <td>Male</td>
+            <td>Females</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+    </tbody>
+</table>');
+
+    }//end testRowspanChangesWhenYouDeleteTheLastRow()
+
+
+    /**
+     * Test that the 'All Genders' rowspan changes when you delete the last row from it.
+     *
+     * @return void
+     */
+    public function testRowspanChangesWhenYouDeleteTheLastRow()
+    {
+        $this->showTools(5, 'row');
+
+        $this->click($this->find($this->getImg('icon_trash.png'), NULL, 0.83));
+
+        $this->assertHTMLMatch('<p>Lorem IPSUM dolor</p><p>sit amet <strong>consectetur</strong></p>
+            <table style="width: 300px;" border="1" cellpadding="2" cellspacing="2">
+            <tbody>
+            <tr>
+            <td style="width: 100px;" colspan="2">&nbsp;Survey&nbsp;</td>
+            <td>All Genders</td>
+            <td style="width: 100px;" colspan="2">By Gender&nbsp;</td>
+            </tr>
+            <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            </tr>
+            <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            </tr>
+            </tbody></table>');
+
+    }//end testRowspanChangesWhenYouDeleteTheLastRow()
 
 
     /**
@@ -197,25 +331,6 @@ class Viper_Tests_ViperTableEditorPlugin_RowUnitTest extends AbstractViperTableE
     }//end testRowInsert5()
 
 
-    /**
-     * Test inserting a row before the table doesn't close the inline toobar.
-     *
-     * @return void
-     */
-    public function testInsertingRowBeforeTheTable()
-    {
-        $this->clickCell(2);
-       $this->type('Add some text to make the cell big so that it causes the bug and some more and more and more and more and more and more text.');
-        
-        $this->showTools(1, 'row');
-
-        $this->click($this->find($this->getImg('icon_insertRowBefore.png'), NULL, 0.80));
-        
-        $this->assertTrue($this->exists($this->getImg('row_tools.png')));
-        
-    }//end testRowInsert5()
-    
-    
 }//end class
 
 ?>
