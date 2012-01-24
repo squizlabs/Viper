d3511f15ef05fc904daee34019b75220b049e02d
Sertan Danis <sdanis@squiz.com.au>
2012-01-24 15:44:51 +1100

    Fixed the way JS files are loaded so that they work on IE8

--- Viper-all.js
+++ Viper-all.js
@@ -19,43 +19,93 @@
  * @copyright  2010 Squiz Pty Ltd (ACN 084 670 600)
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
  */
-var dfxScripts = document.getElementsByTagName('script');
-var path       = null;
+ViperReadyCallback = null;
+(function() {
+    dfxLoadedCallback = function() {
+        var dfxScripts = document.getElementsByTagName('script');
+        var path       = null;
 
-// Loop through all the script tags that exist in the document and find the one
-// that has included this file.
-var dfxScriptsLen = dfxScripts.length;
-for (var i = 0; i < dfxScriptsLen; i++) {
-    if (dfxScripts[i].src) {
-        if (dfxScripts[i].src.match(/Viper-all\.js/)) {
-            // We have found our appropriate <script> tag that includes the
-            // DfxJSLib library, so we can extract the path and include the rest.
-            path = dfxScripts[i].src.replace(/Viper-all\.js/,'');
-            break;
+        // Loop through all the script tags that exist in the document and find the one
+        // that has included this file.
+        var dfxScriptsLen = dfxScripts.length;
+        for (var i = 0; i < dfxScriptsLen; i++) {
+            if (dfxScripts[i].src) {
+                if (dfxScripts[i].src.match(/Viper-all\.js/)) {
+                    // We have found our appropriate <script> tag that includes the
+                    // DfxJSLib library, so we can extract the path and include the rest.
+                    path = dfxScripts[i].src.replace(/Viper-all\.js/,'');
+                    break;
+                }
+            }
         }
-    }
-}
 
-// Viper core files.
-var jsFiles = 'Viper.js|ViperChangeTracker.js|ViperTools.js|ViperDOMRange.js|ViperIERange.js|ViperMozRange.js|ViperSelection.js|ViperPluginManager.js|ViperHistoryManager.js|XPath.js';
-jsFiles     = jsFiles.split('|');
-for (var j = 0; j < jsFiles.length; j++) {
-    document.write('<script type="text/javascript" src="' + path + '/Lib/' + jsFiles[j] + '"></script>');
-}
+        var _loadScript = function(path, scriptName, callback, scriptNameAsPath) {
+            var script = document.createElement('script');
+            script.onreadystatechange = function() {
+                if (/^(loaded|complete)$/.test(this.readyState) === true) {
+                    callback.call(window);
+                }
+            };
 
-document.write('<style type="text/css">@import url("' + path + '/Css/viper.css");</style>');
+            script.onload = function() {
+                callback.call(window);
+            };
 
-// Viper default plugins.
-var plugins    = 'ViperCopyPastePlugin|ViperCoreStylesPlugin|ViperFormatPlugin|ViperKeyboardEditorPlugin|ViperListPlugin|ViperHistoryPlugin|ViperTableEditorPlugin|ViperToolbarPlugin|ViperTrackChangesPlugin|ViperInlineToolbarPlugin|ViperLinkPlugin|ViperAccessibilityPlugin|ViperSourceViewPlugin|ViperImagePlugin|ViperSearchReplacePlugin|ViperAcronymPlugin|ViperAbbrPlugin|ViperLangPlugin|ViperCharMapPlugin|ViperInvisibleCharPlugin';
-plugins        = plugins.split('|');
-var pluginCss  = [];
-var c = 0;
-for (var j = 0; j < plugins.length; j++) {
-    document.write('<script type="text/javascript" src="' + path + 'Plugins/' + plugins[j] + '/' + plugins[j] + '.js"></script>');
-    pluginCss.push(plugins[j] + '/' + plugins[j] + '.css');
-    if ((plugins.length - 1) === j || (j !== 0 && (j % 30) === 0)) {
-        document.write('<style type="text/css">@import url("' + path + 'Plugins/' + pluginCss.join('");\n@import url("' + path + '/Plugins/') + '");</style>');
-        pluginCss = [];
-    }
-}
+            if (scriptNameAsPath === true) {
+                script.src = path + scriptName + '/' + scriptName + '.js';
+            } else {
+                script.src = path + scriptName;
+            }
 
+            if (document.head) {
+                document.head.appendChild(script);
+            } else {
+                document.getElementsByTagName('head')[0].appendChild(script);
+            }
+        };
+        var _loadScripts = function(path, scripts, callback, scriptNameAsPath) {
+            if (scripts.length === 0) {
+                callback.call(window);
+                return;
+            }
+
+            var script = scripts.shift();
+            _loadScript(path, script, function() {
+                _loadScripts(path, scripts, callback, scriptNameAsPath);
+            }, scriptNameAsPath);
+        };
+
+        // Viper core files.
+        var jsFiles = 'Viper.js|ViperChangeTracker.js|ViperTools.js|ViperDOMRange.js|ViperIERange.js|ViperMozRange.js|ViperSelection.js|ViperPluginManager.js|ViperHistoryManager.js|XPath.js';
+        jsFiles     = jsFiles.split('|');
+
+        _loadScripts(path + '/Lib/', jsFiles, function() {
+            var plugins    = 'ViperCopyPastePlugin|ViperCoreStylesPlugin|ViperFormatPlugin|ViperKeyboardEditorPlugin|ViperListPlugin|ViperHistoryPlugin|ViperTableEditorPlugin|ViperToolbarPlugin|ViperTrackChangesPlugin|ViperInlineToolbarPlugin|ViperLinkPlugin|ViperAccessibilityPlugin|ViperSourceViewPlugin|ViperImagePlugin|ViperSearchReplacePlugin|ViperAcronymPlugin|ViperAbbrPlugin|ViperLangPlugin|ViperCharMapPlugin|ViperInvisibleCharPlugin';
+            plugins        = plugins.split('|');
+
+            _loadScripts(path + 'Plugins/', plugins, function() {
+                if (ViperReadyCallback) {
+                    ViperReadyCallback.call(window);
+                }
+            }, true);
+
+
+            var coreCSS = 'viper|viper_tools|viper_moz'.split('|');
+            for (var j = 0; j < coreCSS.length; j++) {
+                var link   = document.createElement('link');
+                link.rel   = 'stylesheet';
+                link.media = 'screen';
+                link.href  = path + 'Css/' + coreCSS[j] + '.css';
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+
+            for (var j = 0; j < plugins.length; j++) {
+                var link   = document.createElement('link');
+                link.rel   = 'stylesheet';
+                link.media = 'screen';
+                link.href  = path + 'Plugins/' + plugins[j] + '/' + plugins[j] + '.css';
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+        });
+    };
+}) ();
