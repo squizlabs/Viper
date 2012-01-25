409878692af78b32de847426a85b49debe1b4610
Sertan Danis <sdanis@squiz.com.au>
2012-01-25 11:14:43 +1100

    Added Asset picker button next to URL box

--- Plugins/MatrixLinkPlugin/MatrixLinkPlugin.js
+++ Plugins/MatrixLinkPlugin/MatrixLinkPlugin.js
@@ -1,19 +1,32 @@
 function MatrixLinkPlugin(viper)
 {
-    this.viper = viper;
+    ViperLinkPlugin.call(this, viper);
 
 }
 
 MatrixLinkPlugin.prototype = {
-  //
-  //  init: function()
-  //  {
-  //    //  this._initInlineToolbar();
-  //
-  //     // this._initToolbar();
-  //
-  //  }
+
+    updateInlineToolbar: function(data)
+    {
+        var contents = ViperLinkPlugin.prototype.updateInlineToolbar.call(this, data);
+        if (!contents) {
+            return;
+        }
+
+        var toolbar = this.viper.ViperPluginManager.getPlugin('ViperInlineToolbarPlugin');
+
+        // Add the asset finder icon to the right side of the URL text box.
+        var findAssetBtn = toolbar.createButton('X', false, 'Pick Asset', false, 'matrixAssetPicker', function() {
+            // Show asset picker.
+        });
+
+        // Find the first textbox and add the asset finder button after it.
+        var urlLabel = dfx.getTag('label', contents)[0];
+        dfx.setStyle(urlLabel, 'float', 'left');
+        dfx.insertAfter(urlLabel, findAssetBtn);
+
+    }
 
 };
 
-//dfx.inherits('MatrixLinkPlugin', 'ViperLinkPlugin', true);
+dfx.inherits('MatrixLinkPlugin', 'ViperLinkPlugin', true);
