ffef9ec4870ab4c92510653bd5d1d94de780c12f
Deborah Sherwood <dsherwood@squiz.com.au>
2012-02-06 11:46:34 +1100

    Added tests for italics within a table

--- Tests/ViperCoreStylesPlugin/StylesInTablesUnitTest.php
+++ Tests/ViperCoreStylesPlugin/StylesInTablesUnitTest.php
@@ -7,11 +7,11 @@ class Viper_Tests_ViperCoreStylesPlugin_StylesInTablesUnitTest extends AbstractV
 
 
     /**
-     * Test that style can be applied to a word in a table cell.
+     * Test that bold can be applied to a word in a table cell.
      *
      * @return void
      */
-    public function testStylingInTableMouseSelect()
+    public function testBoldInTableMouseSelect()
     {
         $text = 'XabcX';
         $this->selectText($text);
@@ -21,22 +21,47 @@ class Viper_Tests_ViperCoreStylesPlugin_StylesInTablesUnitTest extends AbstractV
         $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold_active.png'));
         $this->assertEquals('UnaU TiuT <strong>XabcX</strong> Mnu', $this->getHtml('td,th', 3));
 
-        $this->selectText($text, $text);
+        $this->selectText($text);
         $this->clickInlineToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_bold_active.png');
         $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold.png'));
         $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold.png'));
 
         $this->assertEquals('UnaU TiuT XabcX Mnu', $this->getHtml('td,th', 3));
 
-    }//end testStylingInTableMouseSelect()
+    }//end testBoldInTableMouseSelect()
 
 
     /**
-     * Test that style can be applied to a word in a table cell using shortcuts.
+     * Test that italics can be applied to a word in a table cell.
      *
      * @return void
      */
-    public function testStylingInTableShortcut()
+    public function testItalicInTableMouseSelect()
+    {
+        $text = 'XabcX';
+        $this->selectText($text);
+
+        $this->clickInlineToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_italic.png');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertEquals('UnaU TiuT <em>XabcX</em> Mnu', $this->getHtml('td,th', 3));
+
+        $this->selectText($text);
+        $this->clickInlineToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+
+        $this->assertEquals('UnaU TiuT XabcX Mnu', $this->getHtml('td,th', 3));
+
+    }//end testItalicInTableMouseSelect()
+
+
+    /**
+     * Test that bold can be applied to a word in a table cell using shortcuts.
+     *
+     * @return void
+     */
+    public function testBoldInTableShortcut()
     {
         $text = 'XabcX';
         $this->selectText($text);
@@ -46,22 +71,47 @@ class Viper_Tests_ViperCoreStylesPlugin_StylesInTablesUnitTest extends AbstractV
         $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold_active.png'));
         $this->assertEquals('UnaU TiuT <strong>XabcX</strong> Mnu', $this->getHtml('td,th', 3));
 
+        $this->selectText($text);
         $this->keyDown('Key.CMD + b');
-        $this->clickInlineToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_bold_active.png');
         $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold.png'));
         $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold.png'));
 
         $this->assertEquals('UnaU TiuT XabcX Mnu', $this->getHtml('td,th', 3));
 
-    }//end testStylingInTableShortcut()
+    }//end testBoldInTableShortcut()
 
 
     /**
-     * Test that style can be applied to multiple words in a table cell.
+     * Test that italics can be applied to a word in a table cell using shortcuts.
      *
      * @return void
      */
-    public function testStylingInTableMultiWord()
+    public function testItalicsInTableShortcut()
+    {
+        $text = 'XabcX';
+        $this->selectText($text);
+
+        $this->keyDown('Key.CMD + i');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertEquals('UnaU TiuT <em>XabcX</em> Mnu', $this->getHtml('td,th', 3));
+
+        $this->selectText($text);
+        $this->keyDown('Key.CMD + i');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+
+        $this->assertEquals('UnaU TiuT XabcX Mnu', $this->getHtml('td,th', 3));
+
+    }//end testItalicsInTableShortcut()
+
+
+    /**
+     * Test that bold can be applied to multiple words in a table cell.
+     *
+     * @return void
+     */
+    public function testBoldInTableMultiWord()
     {
         $this->selectText('TiuT', 'XabcX');
 
@@ -89,15 +139,51 @@ class Viper_Tests_ViperCoreStylesPlugin_StylesInTablesUnitTest extends AbstractV
         $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold.png'));
         $this->assertEquals('UnaU TiuT<strong> XabcX</strong> Mnu', $this->getHtml('td,th', 3));
 
-    }//end testStylingInTableMultiWord()
+    }//end testBoldInTableMultiWord()
 
 
     /**
-     * Test that style can be applied to whole cell.
+     * Test that italics can be applied to multiple words in a table cell.
      *
      * @return void
      */
-    public function testStylingForWholeCell()
+    public function testItalicInTableMultiWord()
+    {
+        $this->selectText('TiuT', 'XabcX');
+
+        $this->keyDown('Key.CMD + i');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertEquals('UnaU <em>TiuT XabcX</em> Mnu', $this->getHtml('td,th', 3));
+
+        $this->click($this->find('TiuT'));
+        $this->doubleClick($this->find('XabcX'));
+        $this->selectInlineToolbarLineageItem(4);
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+
+        $this->click($this->find('XabcX'));
+        $this->doubleClick($this->find('TiuT'));
+        $this->selectInlineToolbarLineageItem(4);
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+
+        $this->click($this->find('XabcX'));
+        $this->doubleClick($this->find('TiuT'));
+        $this->clickInlineToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+        $this->assertEquals('UnaU TiuT<strong> XabcX</strong> Mnu', $this->getHtml('td,th', 3));
+
+    }//end testItalicInTableMultiWord()
+
+
+    /**
+     * Test that bold can be applied to whole cell.
+     *
+     * @return void
+     */
+    public function testBoldForWholeCell()
     {
         $first  = $this->find('UnaU');
         $second = $this->find('Mnu');
@@ -121,7 +207,39 @@ class Viper_Tests_ViperCoreStylesPlugin_StylesInTablesUnitTest extends AbstractV
         $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_bold.png'));
         $this->assertEquals('UnaU TiuT XabcX Mnu', $this->getHtml('td,th', 3));
 
-    }//end testStylingForWholeCell()
+    }//end testBoldForWholeCell()
+
+
+    /**
+     * Test that italics can be applied to whole cell.
+     *
+     * @return void
+     */
+    public function testItalicForWholeCell()
+    {
+        $first  = $this->find('UnaU');
+        $second = $this->find('Mnu');
+
+        $this->selectText('XabcX');
+        $this->selectInlineToolbarLineageItem(3);
+
+        $this->clickTopToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_italic.png');
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertEquals('<em>UnaU TiuT XabcX Mnu</em>', $this->getHtml('td,th', 3));
+
+        $end = $this->getTopRight($second);
+        $this->setLocation($end, ($this->getX($end) + 10), $this->getY($end));
+        $this->dragDrop($this->getTopLeft($first), $end);
+        $this->assertTrue($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->keyDown('Key.CMD + i');
+        $this->assertFalse($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+        $this->assertFalse($this->inlineToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic_active.png'));
+        $this->assertTrue($this->topToolbarButtonExists(dirname(__FILE__).'/Images/toolbarIcon_italic.png'));
+        $this->assertEquals('UnaU TiuT XabcX Mnu', $this->getHtml('td,th', 3));
+
+    }//end testItalicForWholeCell()
 
 
     /**
