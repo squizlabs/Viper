d6fa2a50ae53c249b5f3fb70087d4ba3648f0665
Sertan Danis <sdanis@squiz.com.au>
2012-02-08 14:07:11 +1100

    Fixed issues with the MatrixLinkPlugin. The Include Summary option now works. Url field no longer shows the ./?a= prefix and the anchor part of the url for internal links

--- Plugins/MatrixLinkPlugin/MatrixLinkPlugin.js
+++ Plugins/MatrixLinkPlugin/MatrixLinkPlugin.js
@@ -17,9 +17,11 @@ MatrixLinkPlugin.prototype = {
         // Call the parent method.
         var contents = ViperLinkPlugin.prototype.getToolbarContent.call(this, idPrefix);
 
-        var self  = this;
-        var tools = this.viper.ViperTools;
-        var main  = tools.getItem(idPrefix + ':link').element;
+        var self        = this;
+        var tools       = this.viper.ViperTools;
+        var main        = tools.getItem(idPrefix + ':link').element;
+        var incSummary  = false;
+        var anchorValue = '';
 
         // Insert asset picker icon next to url field.
         // Insert anchor row after URL field.
@@ -30,9 +32,36 @@ MatrixLinkPlugin.prototype = {
         dfx.setStyle(urlField, 'float', 'left');
         dfx.insertAfter(urlField, assetPicker);
 
+        // Url value may need to be updated if the link is internal.
+        var urlValue = tools.getItem(idPrefix + ':url').getValue();
+        if (urlValue.indexOf('./?a=') === 0) {
+            // Remove the ./?a= prefix.
+            urlValue = urlValue.replace('./?a=', '');
+
+            // Internal URL.
+            dfx.removeClass(main, 'emailLink');
+            dfx.addClass(main, 'internalLink');
+
+            // If the link content has %asset_summary_xx% keyword then check the summary
+            // checkbox.
+            var link = this.getLinkFromRange();
+            if (link && dfx.getHtml(link).match(/%asset_summary_\d+%/)) {
+                incSummary = true;
+            }
+
+            // Anchor value.
+            var anchorIndex = urlValue.indexOf('#');
+            if (anchorIndex >= 0) {
+                anchorValue = urlValue.substr((anchorIndex + 1));
+                urlValue    = urlValue.substr(0, anchorIndex);
+            }
+
+            tools.getItem(idPrefix + ':url').setValue(urlValue);
+        }
+
         // Create anchor field.
-        var anchor = tools.createTextbox(idPrefix + ':anchor', 'Anchor', '', function() {
-            self.updateLink();
+        var anchor = tools.createTextbox(idPrefix + ':anchor', 'Anchor', anchorValue, function() {
+            self.updateLink(idPrefix);
         });
 
         var anchorRow = tools.createRow(idPrefix + ':anchorRow', 'anchorRow');
@@ -59,7 +88,9 @@ MatrixLinkPlugin.prototype = {
         });
 
         // The include summary checkbox.
-        var includeSummary    = tools.createCheckbox(idPrefix + ':includeSummary', 'Include Summary');
+        var includeSummary    = tools.createCheckbox(idPrefix + ':includeSummary', 'Include Summary', incSummary, function() {
+            self.updateLink(idPrefix);
+        });
         var includeSummaryRow = tools.createRow(idPrefix + ':includeSummaryRow', 'includeSummaryRow');
         includeSummaryRow.appendChild(includeSummary);
 
@@ -76,8 +107,10 @@ MatrixLinkPlugin.prototype = {
 
         var href = link.getAttribute('href');
 
+        var assetid = null;
         if (this._isInternalLink(href) === true) {
-            href = './?a=' + href;
+            assetid = href;
+            href    = './?a=' + href;
         }
 
         // Anchor.
@@ -87,6 +120,56 @@ MatrixLinkPlugin.prototype = {
         }
 
         link.setAttribute('href', href);
+
+        // Remove summary keyword.
+        dfx.setHtml(link, dfx.getHtml(link).replace(/%asset_summary_\d+%/, ''));
+
+        // Content of the link may need to change due to the summary keyword.
+        var includeSummary = this.viper.ViperTools.getItem(idPrefix + ':includeSummary').getValue();
+        if (includeSummary === true && assetid) {
+            link.appendChild(document.createTextNode(' %asset_summary_' + assetid + '%'));
+        }
+
+    },
+
+    updateBubbleFields: function(link)
+    {
+        ViperLinkPlugin.prototype.updateBubbleFields.call(this, link);
+
+        var tools = this.viper.ViperTools;
+        var main  = tools.getItem('ViperLinkPlugin:vtp:link').element;
+
+        // Url value may need to be updated if the link is internal.
+        var urlValue = tools.getItem('ViperLinkPlugin:vtp:url').getValue();
+        if (urlValue.indexOf('./?a=') === 0) {
+            // Remove the ./?a= prefix.
+            urlValue = urlValue.replace('./?a=', '');
+
+            // Internal URL.
+            dfx.removeClass(main, 'emailLink');
+            dfx.addClass(main, 'internalLink');
+
+            // If the link content has %asset_summary_xx% keyword then check the summary
+            // checkbox.
+            var link = this.getLinkFromRange();
+            if (link && dfx.getHtml(link).match(/%asset_summary_\d+%/)) {
+                incSummary = true;
+            }
+
+            // Anchor value.
+            var anchorValue = '';
+            var anchorIndex = urlValue.indexOf('#');
+            if (anchorIndex >= 0) {
+                anchorValue = urlValue.substr((anchorIndex + 1));
+                urlValue    = urlValue.substr(0, anchorIndex);
+            }
+
+            tools.getItem('ViperLinkPlugin:vtp:anchor').setValue(anchorValue);
+            tools.getItem('ViperLinkPlugin:vtp:url').setValue(urlValue);
+            tools.getItem('ViperLinkPlugin:vtp:includeSummary').setValue(incSummary);
+        } else {
+            dfx.removeClass(main, 'internalLink');
+        }
 
     },
 
