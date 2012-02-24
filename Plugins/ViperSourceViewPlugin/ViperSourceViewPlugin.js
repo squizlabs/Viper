4a57a35e8086d6dd843463fd7b282208f5e84557
Sertan Danis <sdanis@squiz.com.au>
2012-02-24 13:18:45 +1100

    Added cleanupHTML to ViperSourceViewPlugin. For now it makes sure that elemens that cannot have content are closed properly.

--- Plugins/ViperSourceViewPlugin/ViperSourceViewPlugin.js
+++ Plugins/ViperSourceViewPlugin/ViperSourceViewPlugin.js
@@ -369,11 +369,21 @@ ViperSourceViewPlugin.prototype = {
 
     getContents: function()
     {
+        var html = this.viper.getHtml();
+        html     = this.cleanupHTML(html);
+
         if (window.StyleHTML) {
-            return StyleHTML(this.viper.getHtml());
-        } else {
-            return this.viper.getHtml();
+            html = StyleHTML(html);
         }
+
+        return html;
+
+    },
+
+    cleanupHTML: function(content)
+    {
+        content = content.replace(/<(area|base|basefont|br|hr|input|img|link|meta)((\s+\w+(\s*=\s*(?:".*?"|\'.*?\'|[^\'">\s]+))?)+)?\s*>/ig, "<$1$2 />");
+        return content;
 
     },
 
