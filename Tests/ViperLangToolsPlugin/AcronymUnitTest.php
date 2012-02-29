446d8b228ffa78f7930be40436ad4773ba5bf083
Deborah Sherwood <dsherwood@squiz.com.au>
2012-03-01 09:58:42 +1100

    Added unit tests for the update changes button for acronymn

--- Tests/ViperLangToolsPlugin/AcronymUnitTest.php
+++ Tests/ViperLangToolsPlugin/AcronymUnitTest.php
@@ -17,7 +17,7 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
 
         //$this->assertFalse($this->topToolbarButtonExists($dir.'toolbarIcon_toggle_language.png'), 'Language icon in Top Toolbar should not be active.');
 
-        $text = 'LOREM';
+        $text = 'XuT';
         $this->selectText($text);
         $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language.png');
 
@@ -43,7 +43,7 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
     {
         $dir = dirname(__FILE__).'/Images/';
 
-        $text = 'LOREM';
+        $text = 'XuT';
 
         $this->selectText($text);
         $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language.png');
@@ -52,7 +52,29 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
         $this->type('abc');
         $this->keyDown('Key.ENTER');
 
-        $this->assertHTMLMatch('<p><acronym title="abc">LOREM</acronym> xtn dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+        $this->assertHTMLMatch('<p>LOREM <acronym title="abc">XuT</acronym> dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+
+        $this->click($this->find($text));
+        $this->selectText($text);
+        $this->assertTrue($this->topToolbarButtonExists($dir.'toolbarIcon_toggle_language_highlighted.png'), 'Language icon in Top Toolbar should be active.');
+
+        $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language_highlighted.png');
+        $this->assertTrue($this->topToolbarButtonExists($dir.'toolbarIcon_acronym_active.png'), 'Acronym icon in Top Toolbar should be active.');
+
+        $text = 'dolor';
+
+        $this->selectText($text);
+        $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_acronym.png');
+        $this->clickTopToolbarButton($dir.'input_acronym.png');
+
+        $this->assertTrue($this->topToolbarButtonExists($dir.'toolbarIcon_updateChanges_disabled.png'), 'Update Changes button should be disabled.');
+
+        $this->clickTopToolbarButton($dir.'input_acronym.png');
+        $this->type('def');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_updateChanges.png');
+
+        $this->assertHTMLMatch('<p>LOREM <acronym title="abc">XuT</acronym> <acronym title="def">dolor</acronym></p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
 
         $this->click($this->find($text));
         $this->selectText($text);
@@ -74,6 +96,7 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
         $dir = dirname(__FILE__).'/Images/';
 
         $text = 'LABS';
+        $textLoc = $this->find($text);
 
         $this->selectText($text);
         $this->assertTrue($this->topToolbarButtonExists($dir.'toolbarIcon_toggle_language_active.png'), 'Class icon in Top Toolbar should be active.');
@@ -83,13 +106,67 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
         $this->clickTopToolbarButton($dir.'toolbarIcon_deleteValue_icon.png');
         $this->keyDown('Key.ENTER');
 
-        $this->assertHTMLMatch('<p>LOREM xtn dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz LABS is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz LABS is orsm</p><p>The <em>QUICK</em> brown fox</p>');
 
         $this->click($this->find($text));
         $this->selectText($text);
         $this->assertTrue($this->topToolbarButtonExists($dir.'toolbarIcon_toggle_language.png'), 'Language icon is still active in the Top Toolbar.');
 
+        // Reapply the abbreviation so we can delete it by using the update changes icon
+        $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_acronym.png');
+        $this->clickTopToolbarButton($dir.'input_acronym.png');
+        $this->type('abc');
+        $this->keyDown('Key.ENTER');
+
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+
+        $this->click($textLoc);
+        $this->selectText($text);
+        $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language_highlighted.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_acronym_active.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_deleteValue_icon.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_updateChanges.png');
+
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz LABS is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+
     }//end testRemovingAcronymAttributeFromAWord()
+
+
+    /**
+     * Test that you can edit an acronym that has been applied to a word.
+     *
+     * @return void
+     */
+    public function testEditingAcronym()
+    {
+        $dir = dirname(__FILE__).'/Images/';
+
+        $text    = 'LABS';
+        $textLoc = $this->find($text);
+
+        $this->selectText($text);
+        $this->assertTrue($this->topToolbarButtonExists($dir.'toolbarIcon_toggle_language_active.png'), 'Class icon in Top Toolbar should be active.');
+
+        $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language_highlighted.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_acronym_active.png');
+        $this->clickTopToolbarButton($dir.'input_acronym.png');
+        $this->type('def');
+        $this->keyDown('Key.ENTER');
+
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abcdef">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+
+        $this->click($textLoc);
+        $this->selectText($text);
+        $this->clickTopToolbarButton($dir.'toolbarIcon_toggle_language_highlighted.png');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_acronym_active.png');
+        $this->clickTopToolbarButton($dir.'input_acronym.png');
+        $this->type('ghi');
+        $this->clickTopToolbarButton($dir.'toolbarIcon_updateChanges.png');
+
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abcdefghi">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+
+    }//end testEditingAcronym()
 
 
     /**
@@ -110,7 +187,7 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
         $this->type('abc');
         $this->keyDown('Key.ENTER');
 
-        $this->assertHTMLMatch('<p>LOREM xtn dolor</p><p>sit amet <acronym title="abc"><strong>WoW</strong></acronym></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <acronym title="abc"><strong>WoW</strong></acronym></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <em>QUICK</em> brown fox</p>');
 
     }//end testAddingAcronymToABoldWord()
 
@@ -133,7 +210,7 @@ class Viper_Tests_ViperLangToolsPlugin_AcronymUnitTest extends AbstractViperUnit
         $this->type('abc');
         $this->keyDown('Key.ENTER');
 
-        $this->assertHTMLMatch('<p>LOREM xtn dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <acronym title="abc"><em>QUICK</em></acronym> brown fox</p>');
+        $this->assertHTMLMatch('<p>LOREM XuT dolor</p><p>sit amet <strong>WoW</strong></p><p>Squiz <acronym title="abc">LABS</acronym> is orsm</p><p>The <acronym title="abc"><em>QUICK</em></acronym> brown fox</p>');
 
     }//end testAddingAcronymToAItalicWord()
 
