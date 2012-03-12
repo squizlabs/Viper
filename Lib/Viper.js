0c2c1b80d616b6d3efe626a5df9760bfb6aedd39
Sertan Danis <sdanis@squiz.com.au>
2012-03-13 10:46:33 +1100

    Added 'Viper:clickedOutside' and 'Viper:clickedInside' events

--- Lib/Viper.js
+++ Lib/Viper.js
@@ -3158,7 +3158,7 @@ Viper.prototype = {
             // Ask plugins if its one of their element.
             var pluginName = this.getPluginForElement(target);
             if (!pluginName) {
-                // TODO: Fire clicked outside.
+                return this.fireCallbacks('Viper:clickedOutside', e);
             } else {
                 return this.fireCallbacks('Viper:pluginMouseDown', {
                     pluginName: pluginName,
@@ -3167,7 +3167,7 @@ Viper.prototype = {
             }
         }
 
-        // TODO: Should this fire after mouseDown?
+        this.fireCallbacks('Viper:clickedInside', e);
         this.fireCaretUpdated();
 
         // Mouse down in active element.
