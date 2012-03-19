72849636cb2982dadf55be2717df24e56a299ab6
Sertan Danis <sdanis@squiz.com.au>
2012-03-20 09:43:15 +1100

    Removed license headers

--- Lib/Viper.js
+++ Lib/Viper.js
@@ -1,27 +1,3 @@
-/**
- * Viper is a WYSIWYG that does not require the use of editable regions. Instead
- * it utilises the browser's range API to edit the content directly in the browser.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program as the file license.txt. If not, see
- * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
- *
- * @package    CMS
- * @subpackage Editing
- * @author     Squiz Pty Ltd <products@squiz.net>
- * @copyright  2010 Squiz Pty Ltd (ACN 084 670 600)
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
- */
-
 function Viper(id, options, callback)
 {
     this.id           = id;
--- Plugins/ViperInlineToolbarPlugin/ViperInlineToolbarPlugin.js
+++ Plugins/ViperInlineToolbarPlugin/ViperInlineToolbarPlugin.js
@@ -1,24 +1,3 @@
-/**
- * JS Class for the Viper InlineToolbar Plugin.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program as the file license.txt. If not, see
- * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
- *
- * @package   Viper
- * @author    Squiz Pty Ltd <products@squiz.net>
- * @copyright 2010 Squiz Pty Ltd (ACN 084 670 600)
- * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GPLv2
- */
 function ViperInlineToolbarPlugin(viper)
 {
     this.viper                = viper;
