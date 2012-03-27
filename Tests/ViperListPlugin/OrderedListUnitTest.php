b8013682209c766f53a9011b267bda1938b9892d
Deborah Sherwood <dsherwood@squiz.com.au>
2012-03-27 16:21:44 +1100

    Added unit tests to expose a bug with undo and lists

--- Tests/ViperListPlugin/OrderedListUnitTest.php
+++ Tests/ViperListPlugin/OrderedListUnitTest.php
@@ -875,6 +875,23 @@ class Viper_Tests_ViperListPlugin_OrderedListUnitTest extends AbstractViperListP
     }//end testHeadingIconNotAvailableForList()
 
 
+    /**
+     * Test that after you remove all items from the list, the undo icon is active and that when you click it the list is replaced.
+     *
+     * @return void
+     */
+    public function testClickUndoAfterRemovingList()
+    {
+        $this->click($this->find('oNo'));
+        $this->clickTopToolbarButton(dirname(__FILE__).'/Images/toolbarIcon_orderedList_active.png');
+        $this->assertHTMLMatch('<p>XabcX uuuuuu. VmumV</p><p>cPOc ccccc dddd. TicT</p><p>ajhsd sjsjwi hhhh:</p><p>aaa bbbbb ccccc</p><p>4 oNo templates</p><p>Audit XuT content</p><p>Accessibility audit report</p><p>Recommendations action plan</p><p>Squiz Matrix guide</p><h2>SoD</h2>');
+
+        $this->clickTopToolbarButton(dirname(dirname(__FILE__)).'/Core/Images/undorIcon_active.png');
+        $this->assertHTMLMatch('<p>XabcX uuuuuu. VmumV</p><p>cPOc ccccc dddd. TicT</p><p>ajhsd sjsjwi hhhh:</p><ol><li>aaa bbbbb ccccc</li><li>4 oNo templates</li><li>Audit XuT content</li><li>Accessibility audit report</li><li>Recommendations action plan</li><li>Squiz Matrix guide</li></ol><h2>SoD</h2>');
+
+    }//end testHeadingIconNotAvailableForList()
+
+
 
 }//end class
 
