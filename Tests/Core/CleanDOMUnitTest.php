8319bc6d983edcde1409af87b5ea4344418c830b
Sertan Danis <sdanis@squiz.com.au>
2012-07-16 13:11:31 +1000

    Fixed unit tests

--- Tests/Core/CleanDOMUnitTest.php
+++ Tests/Core/CleanDOMUnitTest.php
@@ -15,15 +15,7 @@ class Viper_Tests_Core_CleanDOMUnitTest extends AbstractViperUnitTest
     {
         $this->execJS('viper.cleanDOM()');
 
-        $expected = '
-<p>test</p>
-<p><img src=""><br><span>test</span><embed src=""></p>
-<ul><li>test</li></ul>
-<br>
-<table><tbody><tr><td></td></tr></tbody></table>
-<table><tbody><tr><td>test</td></tr></tbody></table>
-<h2>test</h2>
-<p><br><embed src=""><a name="test"></a><a id="test2" href="">test</a></p>';
+        $expected = '<p>test</p><p><img src="" /><br /><span>test</span></p><ul><li>test</li></ul><table><tbody><tr><td></td></tr></tbody></table><table><tbody><tr><td>test</td></tr></tbody></table><h2>test</h2><p><br /><a name="test"></a><a id="test2" href="">test</a></p>';
 
         $this->assertHTMLMatch($expected);
 
@@ -39,28 +31,7 @@ class Viper_Tests_Core_CleanDOMUnitTest extends AbstractViperUnitTest
     {
         $this->execJS('viper.cleanDOM(dfx.getTag("p")[3])');
 
-        $expected = '<p></p>
-<p>test</p>
-<p><img src=""><br><span></span><span>test</span><embed src=""></p>
-<div></div>
-<ul></ul>
-<ul>
-    <li></li>
-</ul>
-<ul>
-    <li>test</li>
-</ul>
-<br>
-<table>
-    <tbody><tr><td></td></tr></tbody>
-</table>
-<table>
-    <tbody><tr><td>test</td></tr></tbody>
-</table>
-<h1></h1>
-<h2>test</h2>
-<p><br><embed src=""><a name="test"></a><a id="test2" href="">test</a></p>
-<blockquote></blockquote>';
+        $expected = '<p>test</p><p><img src="" /><br /><span>test</span></p><ul><li>test</li></ul><table><tbody><tr><td></td></tr></tbody></table><table><tbody><tr><td>test</td></tr></tbody></table><h2>test</h2><p><br /><a name="test"></a><a id="test2" href="">test</a></p>';
 
         $this->assertHTMLMatch($expected);
 
