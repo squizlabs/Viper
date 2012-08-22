2c795b39d59c8968228b52bd3ef0e38b49098eb4
Sertan Danis <sdanis@squiz.com.au>
2012-08-22 14:31:45 +1000

    Added new test

--- Tests/ViperTableEditorPlugin/TableHeadersUnitTest.php
+++ Tests/ViperTableEditorPlugin/TableHeadersUnitTest.php
@@ -199,10 +199,11 @@ class Viper_Tests_ViperTableEditorPlugin_TableHeadersUnitTest extends AbstractVi
                     17 => '<table><tfoot><tr><td colspan="2">test</td></tr></tfoot><tbody><tr><td>%1%</td><td>test</td></tr></tbody></table>',
                     18 => '<table><thead><tr><th>test</th><th>test</th></tr></thead><tbody><tr><th>%1%</th><th rowspan="2">test</th></tr><tr><td>test</td></tr></tbody></table>',
                     19 => '<table><thead><tr><th>test</th><th>test</th></tr></thead><tbody><tr><th>test</th><th rowspan="2">%1%</th></tr><tr><td>test</td></tr></tbody></table>',
+                    20 => '<table><thead><tr><th>test</th><th>test</th></tr><tr><th rowspan="2">test</th><th rowspan="2">%1%</th></tr></thead><tbody><tr><td>test</td><td>test</td></tr></tbody></table>',
                    );
 
         $testCount = count($results);
-        for ($i = 18; $i <= $testCount; $i++) {
+        for ($i = 1; $i <= $testCount; $i++) {
             $this->useTest($i);
             $this->click($this->findKeyword(1));
             $this->showTools(NULL, 'row');
