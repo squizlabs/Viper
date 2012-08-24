3f407956347fced92fda3424558355c50d0ba90c
Deborah Sherwood <dsherwood@squiz.com.au>
2012-08-24 10:24:29 +1000

    Moved a test into the correct test file

--- Tests/ViperFormatPlugin/FormatUnitTest.php
+++ Tests/ViperFormatPlugin/FormatUnitTest.php
@@ -1363,20 +1363,33 @@ class Viper_Tests_ViperFormatPlugin_FormatUnitTest extends AbstractFormatsUnitTe
      *
      * @return void
      */
-    public function testNoListConversion()
+    public function testFormatIconInList()
     {
-        $this->selectKeyword(1);
-        $this->selectInlineToolbarLineageItem(1);
 
+        $this->click($this->findKeyword(1));
+        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
+
+        $this->selectKeyword(2);
+        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
         $this->assertFalse($this->inlineToolbarButtonExists('formats'), 'Formats button should not be available');
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats button should not be available');
+
+        $this->selectInlineToolbarLineageItem(1);
+        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
+        $this->assertFalse($this->inlineToolbarButtonExists('formats'), 'Formats button should not be available');
 
         $this->selectInlineToolbarLineageItem(0);
-
+        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
         $this->assertFalse($this->inlineToolbarButtonExists('formats'), 'Formats button should not be available');
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats button should not be available');
 
-    }//end testNoListConversion()
+        $this->keyDown('Key.RIGHT');
+        $this->keyDown('Key.ENTER');
+        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
+
+        $this->keyDown('Key.ENTER');
+        $this->type('New parra');
+        $this->assertTrue($this->topToolbarButtonExists('formats-p', 'active'), 'Formats icon should appear in the top toolbar.');
+
+    }//end testFormatIconInList()
 
 
     /**
--- Tests/ViperListPlugin/AbstractGeneralListUnitTest.php
+++ Tests/ViperListPlugin/AbstractGeneralListUnitTest.php
@@ -52,36 +52,6 @@ abstract class AbstractGeneralListUnitTest extends AbstractViperListPluginUnitTe
     }//end testHeadingIconNotAvailableForList()
 
 
-     /**
-     * Test that the formats icon is not available for a list.
-     *
-     * @return void
-     */
-    public function testFormatsIconNotAvailableForList()
-    {
-        $this->click($this->findKeyword(4));
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
-
-        $this->selectKeyword(5);
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
-
-        $this->selectInlineToolbarLineageItem(1);
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
-
-        $this->selectInlineToolbarLineageItem(0);
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
-
-        $this->keyDown('Key.RIGHT');
-        $this->keyDown('Key.ENTER');
-        $this->assertTrue($this->topToolbarButtonExists('formats', 'disabled'), 'Formats icon should not appear in the top toolbar.');
-
-        $this->keyDown('Key.ENTER');
-        $this->type('New parra');
-        $this->assertTrue($this->topToolbarButtonExists('formats-p', 'active'), 'Formats icon should appear in the top toolbar.');
-
-    }//end testFormatsIconNotAvailableForList()
-
-
     /**
      * Test that the table icon is not available for a list.
      *
