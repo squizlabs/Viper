78629015ef0ec728bdbfef547166bd0006f3e367
Deborah Sherwood <dsherwood@squiz.com.au>
2012-08-30 10:42:58 +1000

    Moved tests into the correct location and created tests to check the status of the list icons in differnet formats

--- Tests/ViperTableEditorPlugin/GeneralTableUnitTest.php
+++ Tests/ViperTableEditorPlugin/GeneralTableUnitTest.php
@@ -61,38 +61,67 @@ class Viper_Tests_ViperTableEditorPlugin_GeneralTableUnitTest extends AbstractVi
 
 
     /**
-     * Test that the Headings icon is not available when in a table element other than TD or TH.
+     * Test that the table icon is not available for a list.
      *
      * @return void
      */
-    public function testHeadingIconNotAvailableForCaptionAndTable()
+    public function testTableIconNotAvailableForList()
     {
-        // Test selection in caption and whole caption.
+        // Check an unordered list
+        $this->click($this->findKeyword(1));
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
+
+        $this->keyDown('Key.SHIFT + Key.RIGHT');
+        $this->assertTrue($this->topToolbarButtonExists('table'), 'Table icon should be active in the top toolbar.');
+
+        $this->keyDown('Key.TAB');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not active in the top toolbar.');
+
         $this->selectKeyword(1);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
 
         $this->selectInlineToolbarLineageItem(1);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
 
         $this->selectInlineToolbarLineageItem(0);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
+
+        $this->keyDown('Key.RIGHT');
+        $this->keyDown('Key.ENTER');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
+
+        $this->keyDown('Key.ENTER');
+        $this->type('New parra');
+        $this->assertTrue($this->topToolbarButtonExists('table'), 'Table icon should appear in the top toolbar.');
+
+        // Check an ordered list
+        $this->click($this->findKeyword(2));
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
+
+        $this->keyDown('Key.SHIFT + Key.RIGHT');
+        $this->assertTrue($this->topToolbarButtonExists('table'), 'Table icon should be active in the top toolbar.');
+
+        $this->keyDown('Key.TAB');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not active in the top toolbar.');
 
         $this->selectKeyword(2);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
-
-        $this->selectInlineToolbarLineageItem(3);
-        $this->assertTrue($this->topToolbarButtonExists('headings'), 'Headings icon should be enabled.');
-
-        $this->selectInlineToolbarLineageItem(2);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
 
         $this->selectInlineToolbarLineageItem(1);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
 
         $this->selectInlineToolbarLineageItem(0);
-        $this->assertTrue($this->topToolbarButtonExists('headings', 'disabled'), 'Headings icon should be disabled.');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
 
-    }//end testHeadingIconNotAvailableForCaptionAndTable()
+        $this->keyDown('Key.RIGHT');
+        $this->keyDown('Key.ENTER');
+        $this->assertTrue($this->topToolbarButtonExists('table', 'disabled'), 'Table icon should not appear in the top toolbar.');
+
+        $this->keyDown('Key.ENTER');
+        $this->type('New parra');
+        $this->assertTrue($this->topToolbarButtonExists('table'), 'Table icon should appear in the top toolbar.');
+
+    }//end testTableIconNotAvailableForList()
 
 
     /**
