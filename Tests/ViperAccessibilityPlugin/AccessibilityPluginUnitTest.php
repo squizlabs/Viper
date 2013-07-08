a45fb8cceeb94558d33d31c252225007f5d6ae13
Deborah Sherwood <dsherwood@squiz.com.au>
2013-07-08 14:20:30 +1000

    Added unit tests for issue 2815

--- Tests/ViperAccessibilityPlugin/AccessibilityPluginUnitTest.php
+++ Tests/ViperAccessibilityPlugin/AccessibilityPluginUnitTest.php
@@ -13,6 +13,7 @@ class Viper_Tests_ViperAccessibilityPlugin_AccessibilityPluginUnitTest extends A
      */
     public function testOpeningAccessibilityAuditor()
     {
+        $this->useTest(1);
 
         $this->click($this->findKeyword(1));
         $this->assertTrue($this->topToolbarButtonExists('accessAudit'), 'Accessibility auditor icon should be active.');
@@ -51,6 +52,8 @@ class Viper_Tests_ViperAccessibilityPlugin_AccessibilityPluginUnitTest extends A
      */
     public function testViewingSourceFromAccessibilityAuditor()
     {
+        $this->useTest(1);
+
         $this->click($this->findKeyword(1));
         $this->clickTopToolbarButton('accessAudit');
 
@@ -72,6 +75,46 @@ class Viper_Tests_ViperAccessibilityPlugin_AccessibilityPluginUnitTest extends A
         $this->assertTrue($this->buttonExists('Apply Changes', NULL, TRUE), 'Source view did not appear');
 
     }//end testViewingSourceFromAccessibilityAuditor()
+
+
+    /**
+     * Test the carret position in the accessibility auditor.
+     *
+     * @return void
+     */
+    public function testCaretPositionInAccessibilityAuditor()
+    {
+        $this->useTest(2);
+        
+        $this->click($this->findKeyword(1));
+        $this->clickTopToolbarButton('accessAudit');
+
+        // View Report.
+        $viewReportButton = $this->findImage('HTMLCSViewReport', '#HTMLCS-settings-view-report');
+        $this->click($viewReportButton);
+
+        // Click error.
+        $warningIcon = $this->findImage('HTMLCS-report-error', '.HTMLCS-issue-type.HTMLCS-error');
+        $this->click($warningIcon);
+
+        // Click alt field.
+        $this->clickField('Alt');
+        $this->type('alt');
+
+        // Click title field.
+        $this->clickField('Title');
+        $this->type('test');
+        sleep(2);
+
+        // Click Apply Changes.
+        $bubble = $this->getActiveBubble();
+        $this->clickButton('Apply Changes', NULL, TRUE, $bubble);
+        sleep(2);
+
+        $this->assertHTMLMatch('<p>%1%</p><p><img src="http://placebox.es/150/150/666666/f1f1f1/" alt="alt" title="Link title texttest" /></p>');
+
+
+    }//end testCaretPositionInAccessibilityAuditor()
 
 
 }//end class
