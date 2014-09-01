2beaa57b2a23ae423e5b9a16ccbbc104dae873e4
Deborah Sherwood <dsherwood@squiz.com.au>
2014-09-01 13:36:31 +1000

    Added a test for issue 5190

--- Tests/ViperCopyPastePlugin/CopyPasteUnitTest.php
+++ Tests/ViperCopyPastePlugin/CopyPasteUnitTest.php
@@ -246,6 +246,29 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteUnitTest extends AbstractViperUn
 
     }//end testCopyPasteHeading()
 
+
+    /**
+     * Test copy and pasting an image.
+     *
+     * @return void
+     */
+    public function testCopyPasteImage()
+    {
+        $this->useTest(4);
+
+        $this->clickElement('img', 0);
+        $this->sikuli->keyDown('Key.CMD + c');
+        sleep(1);
+        $this->moveToKeyword(1, 'right');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->sikuli->keyDown('Key.CMD + v');
+        sleep(1);
+        $this->assertHTMLMatch('<p>First paragraph</p><img src="%url%/ViperImagePlugin/Images/html-codesniffer.png" alt="Alt tag" /><p>This is the second paragraph in the content of the page %1%</p><p></p><img src="%url%/ViperImagePlugin/Images/html-codesniffer.png" alt="Alt tag" />');
+
+    }//end testCopyPasteImage()
+
+
+
 }//end class
 
 ?>
