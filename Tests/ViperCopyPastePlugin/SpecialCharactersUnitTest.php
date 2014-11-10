c3647c9f6b845f9bb04134442484d4a4e23ee203
Deborah Sherwood <dsherwood@squiz.com.au>
2014-11-10 11:52:57 +1100

    Refactored unit test

--- Tests/ViperCopyPastePlugin/SpecialCharactersUnitTest.php
+++ Tests/ViperCopyPastePlugin/SpecialCharactersUnitTest.php
@@ -6,136 +6,62 @@ class Viper_Tests_ViperCopyPastePlugin_SpecialCharactersUnitTest extends Abstrac
 {
 
 	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for Firefox on OSX.
+     * Test that copying/pasting from the SpecialCharacters doc works correctly.
      *
      * @return void
      */
-    public function testSpecialCharactersForOSXFirefox()
-    {
-        $this->runTestFor('osx', 'firefox');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/MacFirefox/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForOSXFirefox()
-
-
-	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for Chrome on OSX.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForOSXChrome()
-    {
-        $this->runTestFor('osx', 'chrome');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/MacGoogleChrome/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForOSXChrome()
-
-
-	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for Safari on OSX.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForOSXSafari()
-    {
-        $this->runTestFor('osx', 'safari');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/MacSafari/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForOSXSafari()
- 
-
-  	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for Firefox on Windows.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForWindowsFirefox()
-    {
-        $this->runTestFor('windows', 'firefox');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsFirefox/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForWindowsFirefox()
- 
-
- 	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for Chrome on Windows.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForWindowsChrome()
-    {
-        $this->runTestFor('windows', 'chrome');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsGoogleChrome/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForWindowsChrome()
-
-	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for IE8 on Windows.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForWindowsIE8()
-    {
-        $this->runTestFor('windows', 'ie8');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE8/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForWindowsIE8()
-
-
-	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for IE9 on Windows.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForWindowsIE9()
-    {
-        $this->runTestFor('windows', 'ie9');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE9/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForWindowsIE9()
-
-
-	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for IE10 on Windows.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForWindowsIE10()
-    {
-        $this->runTestFor('windows', 'ie10');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE10/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForWindowsIE10()
-
-	/**
-     * Test that copying/pasting from the SpecialCharacters works correctly for IE11 on Windows.
-     *
-     * @return void
-     */
-    public function testSpecialCharactersForWindowsIE11()
-    {
-        $this->runTestFor('windows', 'ie11');
-        $this->_runTest($this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE11/SpecialCharacters.txt'));
-
-    }//end testSpecialCharactersForWindowsIE11()
-
-
- 	/**
-     * Run the test for the SpecialCharacters document.
-     *
-     * @param string $textFileURL The URL of the text file to use in the Unit Test.
-     *
-     * @return void
-     */
-    private function _runTest($textFileURL)
+    public function testSpecialCharacters()
     {
         $this->useTest(1);
+
+        $testFile = '';
+
+        switch ($this->sikuli->getOS()) {
+            case 'osx':
+                switch ($this->sikuli->getBrowserid()) {
+                    case 'firefox':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/MacFirefox/SpecialCharacters.txt');
+                        break;
+                    case 'chrome':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/MacGoogleChrome/SpecialCharacters.txt');
+                        break;
+                    case 'safari':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/MacSafari/SpecialCharacters.txt');
+                        break;
+                    default:
+                        throw new Exception('Browser is not supported on osx');
+                }//end switch
+            case 'windows':
+                switch ($this->sikuli->getBrowserid()) {
+                    case 'firefox':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsFirefox/SpecialCharacters.txt');
+                        break;
+                    case 'chrome':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsGoogleChrome/SpecialCharacters.txt');
+                        break;
+                    case 'ie8':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE8/SpecialCharacters.txt');
+                        break;
+                    case 'ie9':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE9/SpecialCharacters.txt');
+                        break;
+                    case 'ie10':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE10/SpecialCharacters.txt');
+                        break;
+                    case 'ie11':
+                        $testFile = $this->getTestURL('/ViperCopyPastePlugin/TextFiles/WindowsIE11/SpecialCharacters.txt');
+                        break;
+                    default:
+                        throw new Exception('Browser is not supported on windows');
+                }//end switch
+        }//end switch
+
         $this->selectKeyword(1);
-        $this->pasteFromURL($textFileURL);
+        $this->pasteFromURL($testFile);
 
        $this->assertHTMLMatch('<p>~ ! @ # $ % ^ &amp; * ( ) _ +</p><p>` 1 2 3 4 5 6 7 8 9 0 - =</p><p>{ } |</p><p>:</p><p>&lt; &gt; ?</p><p>[ ]</p><p>; "a" \'b\'</p><p>, . /</p><p>&hellip;</p><p>q w e r t y u i o p</p><p>Q W E R T Y U I O P</p><p>a s d f g h j k l</p><p>A S D F G H J K L</p><p>z x c v b n m</p><p>Z X C V B N M</p>');
 
-    }//end _runTest()
+    }//end testSpecialCharacters()
 
 }//end class
 
