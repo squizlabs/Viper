952cab77ad046738cb89fb365557f9fb45a0aa91
Deborah Sherwood <dsherwood@squiz.com.au>
2014-11-26 16:46:43 +1100

    Added unit tests for the cursor assist

---
+++ Tests/ViperCursorAssistPlugin/CursorAssistForImagesUnitTest.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'AbstractViperUnitTest.php';
+
+class Viper_Tests_ViperCursorAssistPlugin_CursorAssistForImagesUnitTest extends AbstractViperUnitTest
+{
+
+
+    /**
+     * Test that the cursor assit line appears above an image and that you can click it.
+     *
+     * @return void
+     */
+    public function testCursorAssistAboveImage()
+    {
+        $this->useTest(1);
+
+        $this->moveToKeyword(1);
+
+        $this->moveMouseToElement('img', 'top');
+
+        // Check to see if the cursor assit line appears above the list
+        $this->assertTrue($this->isCursorAssistLineVisible('img', 'top'));
+
+        // Click the cursor assit line and add new content above list
+        $this->clickCursorAssistLine();
+        $this->type('New content above the image');
+        $this->assertHTMLMatch('<p>New content above the list</p><p><img src="%url%/ViperImagePlugin/Images/html-codesniffer.png" alt=""/></p><p>%1%</p>');
+
+    }//end testCursorAssistAboveImage()
+
+
+    /**
+     * Test that the cursor assit line appears below an image and that you can click it.
+     *
+     * @return void
+     */
+    public function testCursorAssistBelowImage()
+    {
+        $this->useTest(2);
+        
+        $this->moveToKeyword(1);
+
+        $this->moveMouseToElement('img', 'bottom');
+
+        // Check to see if the cursor assit line appears above the list
+        $this->assertTrue($this->isCursorAssistLineVisible('img', 'bottom'));
+
+        // Click the cursor assit line and add new content above list
+        $this->clickCursorAssistLine();
+        $this->type('New content below the image');
+        $this->assertHTMLMatch('<p>%1%</p><p><img src="%url%/ViperImagePlugin/Images/html-codesniffer.png" alt=""/></p><p>New content above the list</p>');
+
+    }//end testCursorAssistBelowImage()
+
+}//end class
+
+?>
