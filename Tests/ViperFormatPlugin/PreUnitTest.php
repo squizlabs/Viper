8133721cacb60061477aa7f7a22a4cb8dd0862c0
Deborah Sherwood <dsherwood@squiz.com.au>
2014-12-10 13:55:30 +1100

    Slow down unit test

--- Tests/ViperFormatPlugin/PreUnitTest.php
+++ Tests/ViperFormatPlugin/PreUnitTest.php
@@ -192,7 +192,7 @@ class Viper_Tests_ViperFormatPlugin_PreUnitTest extends AbstractFormatsUnitTest
         // Check that when you click the active pre icon in the inline toolbar, it is changed to paragraph.
         $this->useTest(1);
         $this->selectKeyword(1);
-        sleep(1);
+        sleep(2);
         $this->selectInlineToolbarLineageItem(0);
         $this->clickInlineToolbarButton('formats-pre', 'active');
         $this->clickInlineToolbarButton('PRE', 'active', TRUE);
