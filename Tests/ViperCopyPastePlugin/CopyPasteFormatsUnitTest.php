15deacaf87005c2ba449d4b9d62300dfdcccf1c5
Deborah Sherwood <dsherwood@squiz.com.au>
2014-12-22 10:52:36 +1100

    Fixing unit tests

--- Tests/ViperCopyPastePlugin/CopyPasteFormatsUnitTest.php
+++ Tests/ViperCopyPastePlugin/CopyPasteFormatsUnitTest.php
@@ -19,7 +19,7 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         $this->moveToKeyword(2, 'right');
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(1);
-        $this->assertHTMLMatch('<pre>Lorum this is more content %1% to test %2%%1% to test %2%</pre>');
+        $this->assertHTMLMatch('<p>First paragraph</p><pre>Lorum this is more content %1% to test %2%%1% to test %2%</pre>');
 
         // Test copy and paste inside a div section
         $this->useTest(2);
@@ -37,7 +37,7 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         $this->moveToKeyword(2, 'right');
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(1);
-        $this->assertHTMLMatch('<blockquote><p>Lorum this is more content %1% to test %2%%1% to test %2%</p></blockquote>');
+        $this->assertHTMLMatch('<p>First paragraph</p><blockquote><p>Lorum this is more content %1% to test %2%%1% to test %2%</p></blockquote>');
 
         // Test copy and paste inside a p section
         $this->useTest(4);
@@ -69,7 +69,7 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         sleep(2);
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(2);
-        $this->assertHTMLMatch('<pre>Lorum this is more content %1% to test %2%</pre><p>%1% to test %2%</p>');
+        $this->assertHTMLMatch('<p>First paragraph</p><pre>Lorum this is more content %1% to test %2%</pre><p>%1% to test %2%</p>');
 
         // Test copy and paste a div section
         $this->useTest(2);
@@ -89,7 +89,7 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         $this->sikuli->keyDown('Key.ENTER');
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(2);
-        $this->assertHTMLMatch('<blockquote><p>Lorum this is more content %1% to test %2%</p><p>%1% to test %2%</p></blockquote>');
+        $this->assertHTMLMatch('<p>First paragraph</p><blockquote><p>Lorum this is more content %1% to test %2%</p><p>%1% to test %2%</p></blockquote>');
 
         // Test copy and paste a paragraph section
         $this->useTest(4);
@@ -121,7 +121,7 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         $this->sikuli->keyDown('Key.ENTER');
         sleep(1);
         $this->sikuli->keyDown('Key.CMD + v');
-        $this->assertHTMLMatch('<pre>Lorum this is more content %1% to test %2%</pre><pre>Lorum this is more content %1% to test %2%</pre>');
+        $this->assertHTMLMatch('<p>First paragraph</p><pre>Lorum this is more content %1% to test %2%</pre><pre>Lorum this is more content %1% to test %2%</pre>');
 
         // Test copy and paste a div section
         $this->useTest(2);
@@ -141,7 +141,7 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         $this->moveToKeyword(2, 'right');
         $this->sikuli->keyDown('Key.ENTER');
         $this->sikuli->keyDown('Key.CMD + v');
-        $this->assertHTMLMatch('<blockquote><p>Lorum this is more content %1% to test %2%</p></blockquote><blockquote><p>Lorum this is more content %1% to test %2%</p></blockquote>');
+        $this->assertHTMLMatch('<p>First paragraph</p><blockquote><p>Lorum this is more content %1% to test %2%</p></blockquote><blockquote><p>Lorum this is more content %1% to test %2%</p></blockquote>');
 
         // Test copy and paste a paragraph section
         $this->useTest(4);
@@ -173,21 +173,21 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteFormatsUnitTest extends Abstract
         sleep(1);
         $this->type('First paste ');
         $this->sikuli->keyDown('Key.CMD + v');
-        $this->assertHTMLMatch('<pre>Lorum this is more content %1% to test %2% First paste Lorum this is more content %1% to test %2%</pre>');
+        $this->assertHTMLMatch('<p>First paragraph</p><pre>Lorum this is more content %1% to test %2% First paste Lorum this is more content %1% to test %2%</pre>');
 
         // Paste again to make sure the character is still there
         $this->sikuli->keyDown('Key.ENTER');
         sleep(1);
         $this->type('Second paste ');
         $this->sikuli->keyDown('Key.CMD + v');
-        $this->assertHTMLMatch('<pre>Lorum this is more content %1% to test %2% First paste Lorum this is more content %1% to test %2% Second Paste Lorum this is more content %1% to test %2%</pre>');
+        $this->assertHTMLMatch('<p>First paragraph</p><pre>Lorum this is more content %1% to test %2% First paste Lorum this is more content %1% to test %2% Second Paste Lorum this is more content %1% to test %2%</pre>');
 
         // Paste again in a new pre section
         $this->sikuli->keyDown('Key.ENTER');
         $this->sikuli->keyDown('Key.ENTER');
         sleep(1);
         $this->sikuli->keyDown('Key.CMD + v');
-        $this->assertHTMLMatch('<pre>Lorum this is more content %1% to test %2% First paste Lorum this is more content %1% to test %2% Second paste Lorum this is more content %1% to test %2%</pre><pre>Lorum this is more content %1% to test %2%</pre>');
+        $this->assertHTMLMatch('<p>First paragraph</p><pre>Lorum this is more content %1% to test %2% First paste Lorum this is more content %1% to test %2% Second paste Lorum this is more content %1% to test %2%</pre><pre>Lorum this is more content %1% to test %2%</pre>');
 
     }//end testCopyAndPastePreFormat()
 
