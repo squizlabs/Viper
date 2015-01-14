6c62c81beffac7e2e28debf469ce23a9a8f2e86d
Deborah Sherwood <dsherwood@squiz.com.au>
2015-01-14 13:21:23 +1100

    Slowing down the unit test

--- Tests/ViperFormatPlugin/ParagraphUnitTest.php
+++ Tests/ViperFormatPlugin/ParagraphUnitTest.php
@@ -652,6 +652,7 @@ class Viper_Tests_ViperFormatPlugin_ParagraphUnitTest extends AbstractFormatsUni
         // Apply bold formatting to the paragraph
         $this->selectKeyword(1);
         $this->selectInlineToolbarLineageItem(0);
+        sleep(1);
         $this->clickTopToolbarButton('bold');
         $this->assertHTMLMatch('<p><strong>%1% Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac augue mi. Nam risus massa, aliquam non porta vel, lacinia a sapien. Nam iaculis sollicitudin sem, vitae dapibus massa dignissim vitae %2%.</strong></p>');
 
