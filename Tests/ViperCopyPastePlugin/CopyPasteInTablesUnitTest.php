e5a94839fbac32e7ccc3eb0b6ce3e78099798088
Deborah Sherwood <dsherwood@squiz.com.au>
2015-03-09 16:18:59 +1100

    Fixing unit test

--- Tests/ViperCopyPastePlugin/CopyPasteInTablesUnitTest.php
+++ Tests/ViperCopyPastePlugin/CopyPasteInTablesUnitTest.php
@@ -11,6 +11,8 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteInTablesUnitTest extends Abstrac
      */
     public function testCopyAndPasteForTable()
     {
+        $this->useTest(1);
+
         $this->selectKeyword(2);
         sleep(2);
         $this->selectInlineToolbarLineageItem(0);
@@ -37,6 +39,8 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteInTablesUnitTest extends Abstrac
      */
     public function testHtmlTablesInPageCopyPaste()
     {
+        $this->useTest(1);
+
         $this->selectKeyword(1);
         $this->sikuli->keyDown('Key.CMD + a');
         $this->pasteFromURL($this->getTestURL('/ViperCopyPastePlugin/CopyPasteFiles/HtmlTablesInPage.txt'));
@@ -56,21 +60,25 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteInTablesUnitTest extends Abstrac
     public function testCopyAndPasteContentFromTable()
     {
         // Copy and paste content inside table
-        $this->selectKeyword(2);
+        $this->useTest(1);
+        $this->selectKeyword(2, 3);
         sleep(2);
-        $this->selectInlineToolbarLineageItem(3);
         $this->sikuli->keyDown('Key.CMD + c');
         
-        $this->moveToKeyword(3, 'right');
-        $this->sikuli->keyDown('Key.ENTER');
+        $this->moveToKeyword(4, 'right');
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(2);
 
         $this->removeTableHeaders();
         sleep(1);
-        $this->assertHTMLMatch('<p>Lorem %1%</p><p>sit amet <strong>consectetur</strong></p><p>eooee</p><table cellspacing="0" cellpadding="3" border="1"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>sapien %2% vel aliquet</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus <strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec <strong>porta</strong> ante</td><td>sapien vel aliquet %3%<br />sapien %2% vel aliquet</td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
+        $this->assertHTMLMatch('<p>Lorem XAX</p><p>sit amet<strong>consectetur</strong></p><p>eooee</p><table border="1" cellpadding="3" cellspacing="0"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>XBX sapien vel aliquet XCX</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus<strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec<strong>porta</strong> ante</td><td>sapien vel aliquet XDXXBX sapien vel aliquet XCX</td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
 
         // Paste content outside table
+        $this->useTest(1);
+        $this->selectKeyword(2, 3);
+        sleep(2);
+        $this->sikuli->keyDown('Key.CMD + c');
+
         $this->moveToKeyword(1, 'right');
         $this->sikuli->keyDown('Key.ENTER');
         $this->sikuli->keyDown('Key.CMD + v');
@@ -78,9 +86,10 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteInTablesUnitTest extends Abstrac
 
         $this->removeTableHeaders();
         sleep(1);
-        $this->assertHTMLMatch('<p>Lorem %1%</p><p>sapien %2% vel aliquet</p><p>sit amet <strong>consectetur</strong></p><p>eooee</p><table cellspacing="0" cellpadding="3" border="1"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>sapien %2% vel aliquet</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus <strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec <strong>porta</strong> ante</td><td>sapien vel aliquet %3%<br />sapien %2% vel aliquet</td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
+        $this->assertHTMLMatch('<p>Lorem %1%</p><p>%2% sapien vel aliquet %3%</p><p>sit amet <strong>consectetur</strong></p><p>eooee</p><table cellspacing="0" cellpadding="3" border="1"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>%2% sapien vel aliquet %3%</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus <strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec <strong>porta</strong> ante</td><td>sapien vel aliquet %4%</td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
 
         // Copy and paste a row from the table outside the table
+        $this->useTest(1);
         $this->selectKeyword(2);
         sleep(2);
         $this->selectInlineToolbarLineageItem(2);
@@ -93,16 +102,21 @@ class Viper_Tests_ViperCopyPastePlugin_CopyPasteInTablesUnitTest extends Abstrac
 
         $this->removeTableHeaders();
         sleep(1);
-        $this->assertHTMLMatch('<p>Lorem %1%</p><table border="1" style="width: 100%;"><tbody><tr><td>test</td><td>sapien %2% vel aliquet</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr></tbody></table><p>sapien %2% vel aliquet</p><p>sit amet <strong>consectetur</strong></p><p>eooee</p><table cellspacing="0" cellpadding="3" border="1"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>sapien %2% vel aliquet</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus <strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec <strong>porta</strong> ante</td><td>sapien vel aliquet %3%<br />sapien %2% vel aliquet</td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
+        $this->assertHTMLMatch('<p>Lorem %1%</p><table border="1" style="width:100%;"><tbody><tr><td>test</td><td>%2% sapien vel aliquet %3%</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr></tbody></table><p>sit amet<strong>consectetur</strong></p><p>eooee</p><table border="1" cellpadding="3" cellspacing="0"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>%2% sapien vel aliquet %3%</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus<strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec<strong>porta</strong> ante</td><td>sapien vel aliquet %4%</td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
 
-        // Paste the row from the table inside the table        
-        $this->moveToKeyword(3, 'right');
+        // Paste the row from the table inside the table      
+        $this->useTest(1);
+        $this->selectKeyword(2);
+        sleep(2);
+        $this->selectInlineToolbarLineageItem(2);
+        $this->sikuli->keyDown('Key.CMD + c');
+        $this->moveToKeyword(4, 'right');
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(2);
 
         $this->removeTableHeaders();
         sleep(1);
-        $this->assertHTMLMatch('<p>Lorem XAX</p><table border="1" style="width:100%;"><tbody><tr><td>test</td><td>sapien XBX vel aliquet</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr></tbody></table><p>sapien XBX vel aliquet</p><p>sit amet<strong>consectetur</strong></p><p>eooee</p><table border="1" cellpadding="3" cellspacing="0"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>sapien XBX vel aliquet</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus<strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec<strong>porta</strong> ante</td><td><p>sapien vel aliquet XCX</p>testsapien XBX vel aliquet<ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul><p><br />sapien XBX vel aliquet</p></td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
+        $this->assertHTMLMatch('<p>Lorem %1%</p><p>sit amet<strong>consectetur</strong></p><p>eooee</p><table border="1" cellpadding="3" cellspacing="0"><caption><strong>Table 1.2:</strong> The table caption text</caption><thead><tr><th>Col1 Header</th><th>Col2 Header</th><th>Col3 Header</th></tr></thead><tfoot><tr><td colspan="3">This is the table footer</td></tr></tfoot><tbody><tr><td>test</td><td>%2% sapien vel aliquet %3%</td><td><ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td></tr><tr><td>nec porta ante</td><td colspan="2">purus neque luctus<strong><a href="http://www.google.com">ligula</a></strong>, vel molestie arcu</td></tr><tr><td>nec<strong>porta</strong> ante</td><td><p>sapien vel aliquet %4%</p>test%2% sapien vel aliquet %3%<ul><li>purus neque luctus ligula, vel molestie arcu</li><li>purus neque luctus</li><li>vel molestie arcu</li></ul></td><td rowspan="2">purus neque luctus ligula, vel molestie arcu</td></tr><tr><td colspan="2">sapien vel aliquet</td></tr></tbody></table>');
 
     }//end testCopyAndPasteContentFromTable()
 
