774b5fc65d3eb0feb82aa35642ca3ba27f76770b
Deborah Sherwood <dsherwood@squiz.com.au>
2015-03-12 13:31:36 +1100

    added test for issue 6521

--- Tests/ViperLinkPlugin/ImageWithLinkUnitTest.php
+++ Tests/ViperLinkPlugin/ImageWithLinkUnitTest.php
@@ -44,6 +44,37 @@ class Viper_Tests_ViperLinkPlugin_ImageWithLinkUnitTest extends AbstractViperUni
     }//end testLinkingAnImage()
 
 
+     /**
+     * Test link content in a line with an image.
+     *
+     * @return void
+     */
+    public function testLinkingContentInALineWithAnImage()
+    {
+        // Using the inline toolbar
+        $this->useTest(2);
+        $this->selectKeyword(2);
+        $this->clickInlineToolbarButton('link');
+        $this->assertTrue($this->inlineToolbarButtonExists('link', 'selected'), 'Toolbar button icon is not correct');
+        $this->type('www.squizlabs.com');
+        $this->sikuli->keyDown('Key.TAB');
+        $this->type('Squiz Labs');
+        $this->sikuli->keyDown('Key.ENTER');        
+        $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>XAX XuT</p><p><img alt="" height="167" src="http://localhost/~dsherwood/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" width="369" />Lorem ipsum dolor sit amet, consectetur<a href="www.squizlabs.com" title="Squiz Labs">XBX</a> elit. Curabitur lacinia nisl quis mattis laoreet. Donec convallis consectetur dolor. Vestibulum aliquam interdum sapien, eget placerat eros fringilla sed.</p><p>LABS is ORSM</p>');
+
+        // Using the top toolbar
+        $this->useTest(2);
+        $this->selectKeyword(2);
+        $this->clickTopToolbarButton('link');
+        $this->type('www.squizlabs.com');
+        $this->sikuli->keyDown('Key.TAB');
+        $this->type('Squiz Labs');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>XAX XuT</p><p><img alt="" height="167" src="http://localhost/~dsherwood/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" width="369" />Lorem ipsum dolor sit amet, consectetur<a href="www.squizlabs.com" title="Squiz Labs">XBX</a> elit. Curabitur lacinia nisl quis mattis laoreet. Donec convallis consectetur dolor. Vestibulum aliquam interdum sapien, eget placerat eros fringilla sed.</p><p>LABS is ORSM</p>');
+
+    }//end testLinkingContentInALineWithAnImage()
+
+
 }//end class
 
 ?>
