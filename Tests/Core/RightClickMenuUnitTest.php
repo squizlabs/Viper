60dbb9db14e6aa2b0e5ed03a49f8321224c8b44c
Deborah Sherwood <dsherwood@squiz.com.au>
2015-06-29 11:31:05 +1000

    Trying to fix unit test for IE11

--- Tests/Core/RightClickMenuUnitTest.php
+++ Tests/Core/RightClickMenuUnitTest.php
@@ -13,12 +13,16 @@ class Viper_Tests_Core_RightClickMenuUnitTest extends AbstractViperUnitTest
     public function testCutMenuItem()
     {
         $this->selectKeyword(1);
+        sleep(1);
         $this->cut(true);
+        sleep(1);
         $this->assertHTMLMatch('<p>EIB MOZ %2%</p>');
 
         $this->moveToKeyword(2, 'right');
+        sleep(1);
         $this->sikuli->keyDown('Key.ENTER');
         $this->sikuli->keyDown('Key.CMD + v');
+        sleep(1);
         $this->assertHTMLMatch('<p>EIB MOZ %2%</p><p>%1%</p>');
 
     }//end testCutMenuItem()
@@ -32,12 +36,16 @@ class Viper_Tests_Core_RightClickMenuUnitTest extends AbstractViperUnitTest
     public function testCopyMenuItem()
     {
         $this->selectKeyword(1);
+        sleep(1);
         $this->copy(true);
+        sleep(1);
         $this->assertHTMLMatch('<p>%1%</p><p>EIB MOZ %2%</p>');
 
         $this->moveToKeyword(2, 'right');
+        sleep(1);
         $this->sikuli->keyDown('Key.ENTER');
         $this->sikuli->keyDown('Key.CMD + v');
+        sleep(1);
         $this->assertHTMLMatch('<p>%1%</p><p>EIB MOZ %2%</p><p>%1%</p>');
 
     }//end testCopyMenuItem()
