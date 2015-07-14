09a29c934ceac9aaa2b9cea56b23c87800e920fe
Sertan Danis <sdanis@squiz.com.au>
2015-07-14 15:38:09 +1000

    Latest Viper

--- viper.js
+++ viper.js
@@ -24523,6 +24523,13 @@ ViperAccessibilityPlugin.prototype = {
         } else {
             document.getElementsByTagName('head')[0].appendChild(link);
         }
+    },
+
+    remove: function()
+    {
+        // Remove plugin buttons.
+        this.viper.ViperTools.removeItem('accessibility');
+
     }
 
 };
