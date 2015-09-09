0942c5a777bc44d37c604899c92e346d8d6272a4
Deborah Sherwood <dsherwood@squiz.com.au>
2015-09-09 11:48:54 +1000

    Fixed test

--- Tests/ViperCopyPastePlugin/CopyAndCutFormatsUnitTest.php
+++ Tests/ViperCopyPastePlugin/CopyAndCutFormatsUnitTest.php
@@ -247,11 +247,11 @@ class Viper_Tests_ViperCopyPastePlugin_CopyAndCutFormatsUnitTest extends Abstrac
         $this->type(' ');
         $this->sikuli->keyDown('Key.CMD + v');
         sleep(1);
-        $this->assertHTMLMatch('<p>First paragraph<strong>%1% bold content %2%</strong></p><pre>&lt;strong&gt;%1% bold content %2%&lt;/strong&gt;<strong>%1% bold content %2% &lt;strong&gt;%1% bold content %2%&lt;/strong&gt;</strong></pre><pre>Lorum this is more content %3% to test</pre>');
+        $this->assertHTMLMatch('<p>First paragraph<strong>XAX bold content XBX</strong></p><pre>&lt;strong&gt;XAX bold content XBX&lt;/strong&gt;<strong>XAX bold content XBX</strong> &lt;strong&gt;XAX bold content XBX&lt;/strong&gt;</pre><pre>Lorum this is more content XCX to test</pre>');
         $this->clickTopToolbarButton('PRE', 'active', TRUE);
         sleep(1);
-        $this->assertHTMLMatch('<p>First paragraph<strong>%1% bold content %2%</strong></p><p>&lt;strong&gt;%1% bold content %2%&lt;/strong&gt;<strong>%1% bold content %2% &lt;strong&gt;%1% bold content %2%&lt;/strong&gt;</strong></p><pre>Lorum this is more content %3% to test</pre>');
-        
+        $this->assertHTMLMatch('<p>First paragraph<strong>XAX bold content XBX</strong></p><p>&lt;strong&gt;XAX bold content XBX&lt;/strong&gt;<strong>XAX bold content XBX</strong> &lt;strong&gt;XAX bold content XBX&lt;/strong&gt;</p><pre>Lorum this is more content XCX to test</pre>');
+
     }//end testPastingHtmlIntoPre()
 
 
