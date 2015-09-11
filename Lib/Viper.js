5495c9a3e2612d0e764ad044e50f19db33137379
Sertan Danis <sdanis@squiz.com.au>
2015-09-11 16:28:05 +1000

    Dont need the value check

--- Lib/Viper.js
+++ Lib/Viper.js
@@ -1357,7 +1357,7 @@ Viper.prototype = {
                         self._valuesRetrievedCallback.call(self);
                     }
                 }
-            } else if (value) {
+            } else {
                 element.setAttribute(attribute, value);
             }//end if
         }//end if
