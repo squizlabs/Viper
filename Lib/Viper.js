841faab6a7d0cd87610f783d0e7e4dd78b02cd1f
Sertan Danis <sdanis@squiz.com.au>
2015-09-15 14:10:52 +1000

    It seems like we no longer need this. Preventing default browser action for CMD + left/right also disabled caret jump to start/end of line.

--- Lib/Viper.js
+++ Lib/Viper.js
@@ -4656,14 +4656,6 @@ Viper.prototype = {
                 self.fireSelectionChanged();
             }, 10);
             return true;
-        } else if ((e.which === 37 || e.which === 39) && (e.metaKey === true && ViperUtil.isOS('mac') === true)) {
-            // Prevent browser history triger on OSX.
-            ViperUtil.preventDefault(e);
-            return false;
-        } else if ((e.which === 37 || e.which === 39) && (e.altKey === true && (ViperUtil.isOS('windows') === true || ViperUtil.isOS('linux') === true))) {
-            // Prevent browser history triger on Windows and Linux.
-            ViperUtil.preventDefault(e);
-            return false;
         }//end if
 
     },
