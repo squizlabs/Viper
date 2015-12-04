c22977804d0ed67a1a5f4708f133cf3460eaafba
Sertan Danis <sdanis@squiz.com.au>
2015-12-04 15:13:09 +1100

    Allow blank attributes..

--- Lib/Viper.js
+++ Lib/Viper.js
@@ -6007,7 +6007,7 @@ Viper.prototype = {
 
     _closeStubTags: function (content)
     {
-        var re  = /<(area|base|basefont|br|hr|input|img|link|meta|embed|viper:param|param)((\s+\w+(\s*=\s*(?:"[^">]+"|\'[^\'>]+\'))?)+)?\s*>/ig;
+        var re  = /<(area|base|basefont|br|hr|input|img|link|meta|embed|viper:param|param)((\s+\w+(\s*=\s*(?:"[^">]*"|\'[^\'>]+\'))?)+)?\s*>/ig;
         content = content.replace(re, "<$1$2 />");
         return content;
 
