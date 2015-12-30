b472027124030474059253758527a37853311515
Deborah Sherwood <dsherwood@squiz.com.au>
2015-12-31 10:05:36 +1100

    Fixed test

--- Tests/ViperReplacementPlugin/ListsWithKeywordsUnitTest.php
+++ Tests/ViperReplacementPlugin/ListsWithKeywordsUnitTest.php
@@ -125,7 +125,7 @@ class Viper_Tests_ViperReplacementPlugin_ListsWithKeywordsUnitTest extends Abstr
 
         $this->assertHTMLMatch('<p>%1% Test content <img alt="TITLE" src="((prop:url))" /> more test content.%2%</p>');
         $this->assertRawHTMLMatch('<p>%1% Test content<img alt="TITLE" data-viper-src="((prop:url))" src="'.$this->getTestURL('/Web/testImage.png').'"> more test content.%2%</p>');
-        
+
     }//end testImageKeywordUnorderedList()
 
     /**
@@ -141,13 +141,15 @@ class Viper_Tests_ViperReplacementPlugin_ListsWithKeywordsUnitTest extends Abstr
         $this->clickTopToolbarButton('listOL');
 
         $this->assertHTMLMatch('<ol><li>%1% Test content<img alt="TITLE" src="((prop:url))" /> more test content.%2%</li></ol>');
-        $this->assertRawHTMLMatch('<ol><li>%1% Test content<img alt="TITLE" data-viper-src="((prop:url))" src="'.$this->getTestURL('/Web/testImage.png').'" /> more test content.%2%</li></ol>');
+        $this->assertRawHTMLMatch('<ol><li>%1% Test content<img alt="TITLE" data-viper-src="((prop:url))" src="'.$this->getTestURL('/Web/testImage.png').'"> more test content.%2%</li></ol>');
+
         // Test for revert
         $this->selectKeyword(1, 2);
         $this->sikuli->keyDown('Key.SHIFT + Key.RIGHT');
         $this->clickTopToolbarButton('listOL', 'active');
 
         $this->assertHTMLMatch('<p>%1% Test content <img alt="TITLE" src="((prop:url))" /> more test content.%2%</p>');
-        $this->assertRawHTMLMatch('<p>%1% Test content<img alt="TITLE" data-viper-src="((prop:url))" src="'.$this->getTestURL('/Web/testImage.png').'" /> more test content.%2%</p>');
+        $this->assertRawHTMLMatch('<p>%1% Test content<img alt="TITLE" data-viper-src="((prop:url))" src="'.$this->getTestURL('/Web/testImage.png').'"> more test content.%2%</p>');
+        
     }//end testImageKeywordOrderedList()
 }
