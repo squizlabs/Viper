f37cc3ac347fc34f609c3a86d6fe7081e9f20af6
Sertan Danis <sdanis@squiz.com.au>
2016-01-11 10:31:45 +1100

    Fixed unit tests

--- Tests/ViperImagePlugin/GeneralImageUnitTest.php
+++ Tests/ViperImagePlugin/GeneralImageUnitTest.php
@@ -45,7 +45,7 @@ class Viper_Tests_ViperImagePlugin_GeneralImageUnitTest extends AbstractViperIma
         $this->sikuli->keyDown('Key.RIGHT');
         $this->sikuli->keyDown('Key.ENTER');
         $this->type('New paragraph');
-        $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>%1% XuT</p><p><img alt="" height="167" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" width="369" /></p><p>New paragraph</p><p>LABS is ORSM %2%</p>');
+        $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>%1% XuT</p><p><img alt="" height="167" src="%url%/ViperImagePlugin/Images/hero-shot.jpg" width="369" /></p><p>New paragraph</p><p>LABS is ORSM %2%</p>');
 
     }//end testStartingNewParagraphAfterImage()
 
@@ -119,7 +119,7 @@ class Viper_Tests_ViperImagePlugin_GeneralImageUnitTest extends AbstractViperIma
      * @return void
      */
     public function testRequiredFieldsWhenAddingAnImage()
-    { 
+    {
         // Test fields in top toolbar
         $this->useTest(1);
         $this->moveToKeyword(1, 'left');
@@ -131,7 +131,7 @@ class Viper_Tests_ViperImagePlugin_GeneralImageUnitTest extends AbstractViperIma
         $this->type('test');
         sleep(1);
         $this->clicktopToolbarButton('Apply Changes', NULL, TRUE);
-        $this->assertHTMLMatch('<h1>Viper Image Test</h1><p><img alt="test" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg"  />%1% XuT</p><p><img alt="" height="167" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" width="369" /></p><p>LABS is ORSM %2%</p>');
+        $this->assertHTMLMatch('<h1>Viper Image Test</h1><p><img alt="test" src="%url%/ViperImagePlugin/Images/hero-shot.jpg"  />%1% XuT</p><p><img alt="" height="167" src="%url%/ViperImagePlugin/Images/hero-shot.jpg" width="369" /></p><p>LABS is ORSM %2%</p>');
 
     }//end testRequiredFieldsWhenModifiyingAnImage()
 
@@ -203,7 +203,7 @@ class Viper_Tests_ViperImagePlugin_GeneralImageUnitTest extends AbstractViperIma
 
         $this->type('test ');
         $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>%1% XuT</p><p>test <img src="%url%/ViperImagePlugin/Images/hero-shot.jpg" alt="" width="369" height="167" /></p><p>LABS is ORSM %2%</p>');
-                 
+
         // Test after image
         $this->useTest(1);
         $this->moveToKeyword(1, 'right');
@@ -236,9 +236,9 @@ class Viper_Tests_ViperImagePlugin_GeneralImageUnitTest extends AbstractViperIma
         for ($i = 1; $i <= 5; $i++) {
             $this->sikuli->keyDown('Key.BACKSPACE');
         }
-        
+
         $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>XuT %3%</p><p>%1%<img alt="" height="167" src="%url%/ViperImagePlugin/Images/hero-shot.jpg" width="369" /> test %2%</p><p>%4%LABS is ORSM</p>');
-                
+
         // Test content after image
         $this->useTest(2);
         $this->moveToKeyword(2, 'left');
@@ -258,9 +258,9 @@ class Viper_Tests_ViperImagePlugin_GeneralImageUnitTest extends AbstractViperIma
         $this->selectKeyword(3);
         $this->selectInlineToolbarLineageItem(0);
         $this->sikuli->keyDown('Key.BACKSPACE');
-        
+
         $this->assertHTMLMatch('<h1>Viper Image Test</h1><p>%1% test<img alt="" height="167" src="%url%/ViperImagePlugin/Images/hero-shot.jpg" width="369" /> test %2%</p><p>%4%LABS is ORSM</p>');
-                
+
         // Test paragraph after image
         $this->useTest(2);
         $this->selectKeyword(4);
