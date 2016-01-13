2fe5c939aa6caef7da463ffe95ce8de3e8d5ce9f
Ryan Fensom <rfensom@squiz.net>
2016-01-13 14:49:18 +1100

    Fixing Unit tests

--- Tests/Core/BaseTagUnitTest.php
+++ Tests/Core/BaseTagUnitTest.php
@@ -1972,4 +1972,120 @@ class Viper_Tests_Core_BaseTagUnitTest extends AbstractViperUnitTest
 
     }// end testDivBaseTagInputLinkSuperscriptFormat()
 
+
+    /**
+     * Test horizontal rule when there is no default block tag.
+     *
+     * @return void
+     */
+    public function testHorizontalRuleNoBaseTag()
+    {
+
+        $this->useTest(1);
+        $this->sikuli->execJS('viper.setSetting("defaultBlockTag", "")');
+
+        // Test applying after first word of content
+        $this->useTest(26);
+        $this->moveToKeyword(1, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        $this->assertHTMLMatch('%1%<br /><hr /> Test content %2% %3% more test content. %4%'); 
+
+        // Test applying before last word of content
+        $this->useTest(26);
+        $this->moveToKeyword(4, 'left');
+        $this->clickTopToolbarButton('insertHr');
+        $this->assertHTMLMatch('%1% Test content %2% %3% more test content.<br /><hr />%4%');
+
+        // Test applying multiple to content
+        $this->useTest(26);
+        $this->moveToKeyword(1, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        sleep(1);
+        $this->moveToKeyword(2, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        sleep(1);
+        $this->moveToKeyword(4, 'left');
+        $this->clickTopToolbarButton('insertHr');
+        sleep(1);
+        $this->moveToKeyword(3, 'left');
+        $this->clickTopToolbarButton('insertHr');
+        sleep(1);
+        $this->assertHTMLMatch('%1%<br /><hr /> Test content %2%<br /><hr /><br /><hr />%3% more test content.<br /><hr />%4%');
+
+        // Test deleting using delete key
+        $this->useTest(27);
+        $this->moveToKeyword(1, 'right');
+        $this->sikuli->keyDown('Key.DELETE');
+        $this->assertHTMLMatch('%1% Test content %2%<br /><hr /><br /><hr />%3% more test content.<br /><hr />%4%');
+
+        // Test deleting using backspace key
+        $this->moveToKeyword(3, 'left');
+        $this->sikuli->keyDown('Key.BACKSPACE');
+        $this->assertHTMLMatch('%1% Test content %2%<br /><hr /><br />%3% more test content.<br /><hr />%4%');
+
+        // Test br tag
+        $this->useTest(28);
+        $this->moveToKeyword(1, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        $this->assertHTMLMatch('%1%<br /><hr /> Test content.%2%<br />%3% Test content.');
+
+        // Test applying before br tag
+        $this->useTest(28);
+        $this->moveToKeyword(2, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        $this->assertHTMLMatch('%1% Test content.%2%<hr />%3% Test content.');
+
+        // Test applying after br tag
+        $this->useTest(28);
+        $this->moveToKeyword(3, 'left');
+        $this->clickTopToolbarButton('insertHr');
+        $this->assertHTMLMatch('%1% Test content.%2%<hr />%3% Test content.');
+
+        // Test for | placement with br tag
+        $this->useTest(29);
+        $this->moveToKeyword(1, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        $this->assertHTMLMatch('%1%<br /><hr />| Test content.%2%<br />%3% Test content.');
+
+        // Test undo adding hr tag using keyboard shortcuts
+        $this->useTest(26);
+        $this->moveToKeyword(1, 'right');
+        $this->clickTopToolbarButton('insertHr');
+        sleep(1);
+        $this->getOSAltShortcut('Undo');
+        $this->assertHTMLMatch('%1% Test content %2% %3% more test content. %4%');
+
+        // Test redo adding hr tag using keyboard shortcuts
+        $this->getOSAltShortcut('Redo');
+        $this->assertHTMLMatch('%1%<br /><hr /> Test content %2% %3% more test content. %4%');
+        
+    }//end testHorizontalRuleNoBaseTag()
+
+
+    /**
+     * Test horizontal rule when there is no default block tag.
+     *
+     * @return void
+     */
+    public function testCaretWithNoBaseTagList()
+    {
+        $this->useTest(1);
+        $this->sikuli->execJS('viper.setSetting("defaultBlockTag", "")');
+
+        // Test adding to list with enter
+        $this->useTest(30);
+        $this->moveToKeyword(3, 'right');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->type('...');
+        $this->assertHTMLMatch('<li>%1% Test content</li><li>%2% Test content</li><li>%3%</li><li>...</li>');
+
+        // Test adding outside the list with enter
+        $this->useTest(30);
+        $this->moveToKeyword(3, 'right');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->type('...');
+        $this->assertHTMLMatch('<li>%1% Test content</li><li>%2% Test content</li><li>%3%</li>...');
+    }//end testCaretWithNoBaseTag()
+
 }//end class
