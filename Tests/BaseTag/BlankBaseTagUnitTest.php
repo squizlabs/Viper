416281e8cfa87ec1b7aa5f96205f72acb377000c
Deborah Sherwood <dsherwood@squiz.com.au>
2016-01-20 14:04:42 +1100

    Renaming files

--- Tests/BaseTag/BlankBaseTagUnitTest.php
+++ Tests/BaseTag/BlankBaseTagUnitTest.php
@@ -2,7 +2,7 @@
 
 require_once 'AbstractViperUnitTest.php';
 
-class Viper_Tests_BaseTag_BaseTagUnitTest extends AbstractViperUnitTest
+class Viper_Tests_BaseTag_BlankBaseTagUnitTest extends AbstractViperUnitTest
 {
 
 
