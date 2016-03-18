eee90b172541955656bd2fce0a70a6de176a721a
Ryan Fensom <rfensom@squiz.net>
2016-03-18 16:00:12 +1100

    Fixing Unit tests

--- Tests/BlockTag/BlankBlockTagWithImagesUnitTest.php
+++ Tests/BlockTag/BlankBlockTagWithImagesUnitTest.php
@@ -104,4 +104,119 @@ class Viper_Tests_BlockTag_BlankBlockTagWithImagesUnitTest extends AbstractViper
 
     }//end testDeletingImageInBlankBlockTag()
 
+
+    /**
+     * Test applying and removing a link on an image in content with a blank block tag
+     *
+     * @return void
+     */
+    public function testApplyingAndRemovingLinkOnImageInBlankBlockTag()
+    {
+        $this->useTest(1);
+        $this->sikuli->execJS('viper.setSetting("defaultBlockTag", "")');
+
+        // Test using top toolbar
+        // Test applying link
+        $this->useTest(3);
+        $this->clickElement('img', 0);
+        $this->clicktopToolbarButton('link');
+        $this->type('http://www.squizlabs.com');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->assertHTMLMatch('This is %1%<a href="http://www.squizlabs.com"><img alt="test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" /></a> %2% some content');
+
+        // Test removing link
+        $this->clickElement('img', 0);
+        $this->clicktopToolbarButton('linkRemove');
+        $this->assertHTMLMatch('This is %1%<img alt="test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" /> %2% some content');
+
+        // Test using inline toolbar
+        // Test applying link
+        $this->useTest(3);
+        $this->clickElement('img', 0);
+        $this->clickInlineToolbarButton('link');
+        $this->type('http://www.squizlabs.com');
+        $this->sikuli->keyDown('Key.ENTER');
+        $this->assertHTMLMatch('This is %1%<a href="http://www.squizlabs.com"><img alt="test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" /></a> %2% some content');
+
+        // Test removing link
+        $this->clickElement('img', 0);
+        $this->clickInlineToolbarButton('linkRemove');
+        $this->assertHTMLMatch('This is %1%<img alt="test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" /> %2% some content');
+
+    }//end testApplyingAndRemovingLinkOnImageInBlankBlockTag()
+
+
+    /**
+     * Test editing an image alt in content with a blank block tag
+     *
+     * @return void
+     */
+    public function testEditingImageAltInBlankBlockTag()
+    {
+        $this->useTest(1);
+        $this->sikuli->execJS('viper.setSetting("defaultBlockTag", "")');
+
+        // Test using inline toolbar
+        $this->useTest(3);
+        $this->clickElement('img', 0);
+        $this->clickInlineToolbarButton('image', 'active');
+        $this->clickField('Alt');
+        $this->sikuli->keyDown('Key.CMD + a');
+        $this->sikuli->keyDown('Key.LEFT');
+        $this->type('modified-');
+        $this->sikuli->keyDown('Key.ENTER');
+
+        $this->assertHTMLMatch('This is %1%<img alt="modified-test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" /> %2% some content');
+
+        // Test using top toolbar
+        $this->useTest(3);
+        $this->clickElement('img', 0);
+        $this->clicktopToolbarButton('image', 'active');
+        $this->clickField('Alt');
+        $this->sikuli->keyDown('Key.CMD + a');
+        $this->sikuli->keyDown('Key.LEFT');
+        $this->type('modified-');
+        $this->sikuli->keyDown('Key.ENTER');
+
+        $this->assertHTMLMatch('This is %1%<img alt="modified-test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" /> %2% some content');
+
+    }//end testEditingImageAltInBlankBlockTag()
+
+
+    /**
+     * Test editing an image title in content with a blank block tag
+     *
+     * @return void
+     */
+    public function testEditingImageTitleInBlankBlockTag()
+    {
+        $this->useTest(1);
+        $this->sikuli->execJS('viper.setSetting("defaultBlockTag", "")');
+
+        // Test using inline toolbar
+        $this->useTest(4);
+        $this->clickElement('img', 0);
+        $this->clickInlineToolbarButton('image', 'active');
+        $this->clickField('Title');
+        $this->sikuli->keyDown('Key.CMD + a');
+        $this->sikuli->keyDown('Key.LEFT');
+        $this->type('modified-');
+        $this->sikuli->keyDown('Key.ENTER');
+
+        $this->assertHTMLMatch('This is %1%<img alt="test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" title="modified-test_Title" /> %2% some content');
+
+        // Test using top toolbar
+        $this->useTest(4);
+        $this->clickElement('img', 0);
+        $this->clicktopToolbarButton('image', 'active');
+        $this->clickField('Title');
+        $this->sikuli->keyDown('Key.CMD + a');
+        $this->sikuli->keyDown('Key.LEFT');
+        $this->type('modified-');
+        $this->sikuli->keyDown('Key.ENTER');
+
+        $this->assertHTMLMatch('This is %1%<img alt="test_Alt" src="http://localhost/~slabs/Viper/Tests/ViperImagePlugin/Images/hero-shot.jpg" title="modified-test_Title" /> %2% some content');
+
+    }//end testEditingImageTitleInBlankBlockTag()
+
 }//end class
